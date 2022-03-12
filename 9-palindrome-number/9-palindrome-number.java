9830c0fdea50f879b69092d1af0cb2fb3a774e9e
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-03-12 21:08:26 +0530

    Time: 13 ms (63.82%), Space: 45.8 MB (14.45%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isPalindrome(int x) {
         StringBuilder sb = new StringBuilder();
         sb.append(x);
-        boolean abba = false;
         if(sb.length()==1){
             return true;
         }
@@ -12,17 +11,13 @@ class Solution {
         int i=0;
         int j=sb.length()-1;
         while (i<j){
-            if(sb.charAt(i)==sb.charAt(j)){
-                abba = true;
-            }else{
-                abba = false;
-                break;
+            if(sb.charAt(i)!=sb.charAt(j)){
+                return false;
             }
             i++;
             j--;
         }
-
-        return abba;
+        return true;
         
     }
 }
