2ce6f2f9271fcdb49535193bbb2bd79356ffbd7a
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-06-07 14:27:20 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (99.66%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,17 +1,39 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // int[] res = new int[nums1.length];
-//         if(nums1.length == 0){
-//             for(int i=0; i<nums2.length; i++){
-                
-//             }
+        
+//         int i =m-1;
+//         int j = n-1;
+//         int k = m+n-1;
+//         if(n == 0){
+//             return;
+//         }
+//         if(m==0){
+//             // System.out.println(Arrays.toString(nums1));
+//             nums1[0] = nums2[0];
+//             return;
 //         }
         
-        for(int i =m; i < nums1.length; i++){
-            nums1[i] = nums2[i-m];
+        
+//         while(k == 0){
+//             if(nums2[j]>nums1[i]){
+//                 nums1[k] = nums2[j];
+                
+//                 k--;
+//                 j--;
+//             }else{
+                
+//                 nums1[k] = nums1[i];
+//                 i--;
+//                 k--;
+//             }
+//         }
+int index=m+n-1,bp1=m-1,bp2=n-1;  //bp-->back-pointer
+        while(bp1>=0 && bp2>=0){
+            if(nums1[bp1] > nums2[bp2]) nums1[index] = nums1[bp1--];
+            else nums1[index] = nums2[bp2--];
+            index--;
         }
-        Arrays.sort(nums1);
-        System.out.println(Arrays.toString(nums1));
-            
+        while(bp1>=0) nums1[index--] = nums1[bp1--];
+        while(bp2>=0) nums1[index--] = nums2[bp2--];
     }
 }
