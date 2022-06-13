6e177c60458fe606864c7703a88bc765216a6cdf
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-06-14 00:39:10 +0530

    Time: 6 ms (61.03%), Space: 78.9 MB (51.90%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       //    mpist = maximum profit if sold today
+//    leastsf = least so for
+//    dpl = maximum profit if you sell by today or before
+        int mpist = 0;
+        int leastsf = prices[0];
+        int[] dpl = new int[prices.length];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < leastsf) {
+                leastsf = prices[i];
+            }
+            mpist = prices[i] - leastsf;
+            if (mpist > dpl[i - 1]) {
+                dpl[i] = mpist;
+            } else {
+                dpl[i] = dpl[i - 1];
+            }
+        }
+//        mpibt = maximum profit if bought today
+//        maxat = max after today
+//        dpr= maximum profit if you buy by today or later
+        int mpibt = 0;
+        int maxat = prices[prices.length - 1];
+        int dpr[] = new int[prices.length];
+        for (int i = prices.length - 2; i >= 0; i--) {
+              if (prices[i]>maxat){
+                  maxat = prices[i];
+              }
+              mpibt = maxat - prices[i];
+              if (mpibt > dpr[i+1]){
+                  dpr[i] = mpibt;
+              } else {
+                  dpr[i] = dpr[i+1];
+              }
+        }
+        int op = 0;
+        for (int i =0; i< prices.length; i++){
+            if (dpl[i] + dpr[i] > op){
+                op = dpl[i] + dpr[i];
+            }
+        }
+
+        return op;
+    }
+}
