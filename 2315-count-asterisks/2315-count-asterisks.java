94e3dd99e673f080740403dd7fd347f1121d9b61
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-06-25 23:06:51 +0530

    Time: 1 ms (100.00%), Space: 40.4 MB (85.71%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -1,23 +1,33 @@
 class Solution {
     public int countAsterisks(String s) {
         int c =0;
+        boolean flag = false;
         
-        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '*' && !flag ){
+                c++;
+            }else if(ch == '|'){
+                flag = !flag;
+            }
+        }
         
-        for(int i = 0 ; i < s.length() ; i++){
-            st.push(s.charAt(i));
-        }
-       while(!st.isEmpty()){
-            char ch =st.pop();
-           if(ch == '*'){
-               c++;
-           }else if(ch == '|'){
-               while(st.peek() !='|'){
-                   st.pop();
-               }
-               st.pop();
-           }
-        }
+//         Stack<Character> st = new Stack<>();
+        
+//         for(int i = 0 ; i < s.length() ; i++){
+//             st.push(s.charAt(i));
+//         }
+//        while(!st.isEmpty()){
+//             char ch =st.pop();
+//            if(ch == '*'){
+//                c++;
+//            }else if(ch == '|'){
+//                while(st.peek() !='|'){
+//                    st.pop();
+//                }
+//                st.pop();
+//            }
+//         }
         return c;
     }
 }
