58e07e8cc97c465df072ea277ebe41ca59590919
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-06-27 20:21:45 +0530

    Time: 257 ms (33.34%), Space: 229.5 MB (27.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,33 +1,39 @@
 class Solution {
-   boolean found = false;
-    public boolean validPath(int n, int[][] edges, int start, int end) {
-        if(start == end) return  true;
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if(source == destination ) return true;
         
-        Map<Integer,List<Integer>> graph = new HashMap();
-        boolean[] visited = new boolean[n];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         
-        for(int i = 0 ; i < n ; i++) graph.put(i, new ArrayList());
-         //construct graph, add bidirectional vertex
-        for(int[] edge : edges){
-           graph.get(edge[0]).add(edge[1]);
-           graph.get(edge[1]).add(edge[0]);
+        for(int i = 0 ; i < n ; i++){
+            adj.add(new ArrayList<>());
         }
-		//start dfs from start point
-        dfs(graph,visited,start,end);
+        int edge = edges.length;
+        for (int i = 0; i < edge; i++) {
+           
+            int v1 = edges[i][0];
+            int v2 = edges[i][1];
+            adj.get(v1).add(v2);
+            adj.get(v2).add(v1);
+        }
+        boolean[] visited = new boolean[n];
+         dfs( adj, visited , source, destination);
         return found;
+        
     }
+    boolean found = false;
     
-    private void dfs(Map<Integer,List<Integer>> graph,boolean[] visited, int start, int end){
-        if(visited[start] || found) return;
-        visited[start] = true;
+    public void dfs(ArrayList<ArrayList<Integer>> adj,boolean[] visited, int source, int dest){
+        if(visited[source] || found) return;
+        
+        visited[source] = true;
         //when we found and neighbour which is equal to end point inside the recursion, voooleeey! break and return the true
-        for(int nei : graph.get(start)){
-            if(nei == end){
+        for(int nei : adj.get(source)){
+            if(nei == dest){
                 found = true;
                 break;
             }
             if(!visited[nei])
-                dfs(graph, visited, nei, end); //otherwise deep dig again!
+                dfs(adj, visited, nei, dest); //otherwise deep dig again!
         }
     }
 }
