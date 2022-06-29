090aa5c9f0b0ea5e1cf90bcbd1b85b1f7698630a
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-06-29 23:36:03 +0530

    Time: 12 ms (40.10%), Space: 49.3 MB (53.69%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,78 +1,59 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-                ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0 ; i< numCourses; i++){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
-        for (int i =0 ; i < prerequisites.length; i++){
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        for (int[] prerequisite : prerequisites) {
+            adj.get(prerequisite[0]).add(prerequisite[1]);
         }
-        boolean hasCycle = detectCycleDFS(numCourses,adj);
-        if(hasCycle){
-            return new int[0];
-        };
-        int[] indegree = new int[numCourses];
-        int[] topo = new int[numCourses];
+        int [] topolist = topoSortBFS(adj,numCourses);
+        
+        int i = 0, j = topolist.length - 1;
+        while (i < j) {
+            int temp = topolist[i];
+            topolist[i] = topolist[j];
+            topolist[j] = temp;
+            i++;
+            j--;
+        }
+            return topolist;
+        
+    }
+    public  int[] topoSortBFS(ArrayList<ArrayList<Integer>> adj, int V) {
+        int[] indegree = new int[V];
+        int[] topo = new int[V];
 
-        for (int i =0; i < numCourses; i++){
-            for (Integer it : adj.get(i)){
+        for (int i = 0; i < V; i++) {
+            for (Integer it : adj.get(i)) {
                 indegree[it]++;
             }
         }
 
-
         Queue<Integer> que = new LinkedList<>();
-        for (int i =0; i<numCourses; i++){
+        for (int i = 0; i < V; i++) {
             if (indegree[i] == 0) que.add(i);
         }
-
-        int ind =0;
-        while (!que.isEmpty()){
+       int c =0;
+        int ind = 0;
+        while (!que.isEmpty()) {
             Integer node = que.poll();
             topo[ind++] = node;
-
-            for (Integer it : adj.get(node)){
+           c++;
+            for (Integer it : adj.get(node)) {
                 indegree[it]--;
-                if (indegree[it] == 0)
-                {
+                if (indegree[it] == 0) {
                     que.add(it);
                 }
             }
         }
-        int i=0,j = topo.length-1;
-        while (i<j){
-            int temp = topo[i];
-            topo[i] = topo[j];
-            topo[j] = temp;
-            i++;
-            j--;
-        }
-        return topo;
+       if (c < V){
+        // to check for cycle if c is less than v there is a cycle
+           return new int[0];
+       }else{
+        return topo;           
+       }
 
     }
-    public  boolean detectCycleDFS(int V,  ArrayList<ArrayList<Integer>> adj) {
-        boolean[] vis = new boolean[V];
-        boolean[] dfsVis = new boolean[V];
 
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                if (dfs(i, vis, dfsVis,  adj)) return true;
-            }
-        }
-        return false;
-    }
-    public boolean dfs(int src, boolean[] vis, boolean[] dfsVis,  ArrayList<ArrayList<Integer>> adj) {
-
-        vis[src] = true;
-        dfsVis[src] = true;
-
-        for (Integer it : adj.get(src)) {
-            if (!vis[it]) {
-                if (dfs(it, vis, dfsVis,adj)) return true;
-            } else if (dfsVis[it]) {
-                return true;
-            }
-        }
-        dfsVis[src] = false;
-        return false;
-    }}
+}
