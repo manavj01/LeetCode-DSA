f6164c3c2ac498ba35772cd6b1c7afc1bef4e663
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-02 15:02:51 +0530

    Time: 4 ms (67.90%), Space: 55.4 MB (24.98%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,24 +1,27 @@
 class Solution {
    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-					
-        path.add(0);
-        dfsSearch(graph, 0, res, path);
-					
-        return res;
+        if(graph == null || graph.length == 0){ return null; }
+        int n = graph.length;
+        
+        List<Integer> curr = new ArrayList<>();
+        List<List<Integer>> answer = new ArrayList<>();
+        curr.add(0);
+        dfs(graph, 0, n - 1, curr, answer);
+        
+        return answer;
     }
-
-    private void dfsSearch(int[][] graph, int node, List<List<Integer>> res, List<Integer> path) {
-        if (node == graph.length - 1) {
-            res.add(new ArrayList<Integer>(path));
+    
+    private void dfs(int[][] graph, int source, int target, 
+                     List<Integer> curr, List<List<Integer>> answer){
+        if(source == target){
+            answer.add(new ArrayList<>(curr));
             return;
         }
-
-        for (int nextNode : graph[node]) {
-            path.add(nextNode);
-            dfsSearch(graph, nextNode, res, path);
-            path.remove(path.size() - 1);
+        
+        for(int child : graph[source]){
+            curr.add(child);
+            dfs(graph, child, target, curr, answer);
+            curr.remove(curr.size() - 1);
         }
     }
 }
