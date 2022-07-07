bad6a3b938e75297e3e9b5e8422fbe28eb96ca8f
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-07 16:51:08 +0530

    Time: 8 ms (46.24%), Space: 43.5 MB (70.12%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> hm = new HashMap<>();
+        
+//         for(int i =0 ; i < s.length(); i++){
+//           Integer count = hm.get(s.charAt(i));
+//            if (count == null) {
+//            hm.put(s.charAt(i), 1);
+//            }
+//            else {
+//             hm.put(s.charAt(i), count + 1);
+//            }
+//         }
+    char[] orig = new char[26];    
+    char[] anag = new char[26];
+    
+        for(int i =0 ; i < s.length(); i++){
+            orig[s.charAt(i)-'a']++;
+        }
+        for(int i =0 ; i < t.length(); i++){
+            anag[t.charAt(i)-'a']++;
+        }
+        
+       if (Arrays.equals(orig,anag)){
+           return true;
+       }else return false;
+      
+    }
+}
