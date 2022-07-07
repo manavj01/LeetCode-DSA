0ecf726c93aed5f32a7fadb0f7d147df805cc35a
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-07 16:56:00 +0530

    Time: 6 ms (62.50%), Space: 43.4 MB (70.12%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isAnagram(String s, String t) {
        
     int[] orig = new int[26];    
-    // int[] anag = new int[26];
+    int[] anag = new int[26];
     
         for(int i =0 ; i < s.length(); i++){
             orig[s.charAt(i)-'a']++;
@@ -11,11 +11,9 @@ class Solution {
             orig[t.charAt(i)-'a']--;
         }
         
-//        if (Arrays.equals(orig,anag)){
-//            return true;
-//        }else return false;
-      for(int i : orig) if(i != 0)return false;
-        
-        return true;
+       if (Arrays.equals(orig,anag)){
+           return true;
+       }else return false;
+    
     }
 }
