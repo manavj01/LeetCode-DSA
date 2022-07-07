4df60ce6b4a8ead621062a4bd7dce6985decd5a8
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-07 16:53:31 +0530

    Time: 6 ms (62.50%), Space: 43.2 MB (80.09%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -12,18 +12,20 @@ class Solution {
 //            }
 //         }
     int[] orig = new int[26];    
-    int[] anag = new int[26];
+    // int[] anag = new int[26];
     
         for(int i =0 ; i < s.length(); i++){
             orig[s.charAt(i)-'a']++;
         }
         for(int i =0 ; i < t.length(); i++){
-            anag[t.charAt(i)-'a']++;
+            orig[t.charAt(i)-'a']--;
         }
         
-       if (Arrays.equals(orig,anag)){
-           return true;
-       }else return false;
-      
+//        if (Arrays.equals(orig,anag)){
+//            return true;
+//        }else return false;
+      for(int i : orig) if(i != 0)return false;
+        
+        return true;
     }
 }
