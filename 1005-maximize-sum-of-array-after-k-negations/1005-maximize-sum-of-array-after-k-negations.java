63b0d890a72a967bac876558f4f3bcb2f0499ef0
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-09 19:09:30 +0530

    Time: 8 ms (31.29%), Space: 43.5 MB (11.19%) - LeetHub

---
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num : nums){
+            pq.add(num);
+        }
+         int sum=0;
+        
+        for(int i =0 ; i < nums.length; i++){
+            if(k>0  && pq.peek() < 0){
+                int a = pq.poll();
+                pq.add(-a);
+                k--;
+            }
+        }
+        int min = pq.peek();
+        for(int i =0 ; i < nums.length; i++) sum += pq.poll();
+        
+        return sum - k%2 * min* 2 ;
+    }
+}
