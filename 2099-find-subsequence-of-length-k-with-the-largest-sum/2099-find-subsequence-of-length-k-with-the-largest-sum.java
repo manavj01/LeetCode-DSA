8d6eeefbaa95c8be4f5ab07721d969cd9ea52843
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-09 17:05:57 +0530

    Time: 19 ms (12.68%), Space: 45.8 MB (6.34%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -5,9 +5,8 @@ class Solution {
         for(int num : nums){
             if( pq.size()==k){
                 if(pq.peek()<num){
-                     pq.poll();
-                                pq.offer(num);
-
+                    pq.poll();
+                    pq.offer(num);
                 }
             }else{
             pq.offer(num);
@@ -15,19 +14,19 @@ class Solution {
             }
         }
         System.out.println(pq);
-       int[] res = new int[k]; int idx=0;
-        
+       // int[] res = new int[k]; int idx=0;
+        ArrayList<Integer> res = new ArrayList<>(k);
         for(int i =0; i < nums.length; i++){
             if(pq.contains(nums[i])){
-                res[idx++]=nums[i];
+                // res[idx++]=nums[i];
+                res.add(nums[i]);
                 pq.remove(nums[i]);
-                // res.add(num[i]);
             }
         }
         
         
-        // return res.stream().mapToInt(i -> i).toArray() ;
-        return res;
+        return res.stream().mapToInt(i -> i).toArray() ;
+        // return res;
         
     }
 }
