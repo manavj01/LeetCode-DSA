8522594d15f067ff99f454770048b7dcd7c868a3
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-09 17:09:58 +0530

    Time: 10 ms (62.61%), Space: 44.7 MB (61.97%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
-         HashMap<Integer,Queue<Integer>> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.putIfAbsent(nums[i],new LinkedList<>());
-            map.get(nums[i]).add(i);
+         PriorityQueue<Integer> pq = new PriorityQueue<>(k + 1);
+        for (int n : nums) {
+            pq.offer(n);
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
-        Arrays.sort(nums);
-        int a[][] = new int[k][2];
-        int index=0;
-        for(int i=nums.length-k;i<nums.length;i++){
-            a[index][0] = nums[i];
-            a[index++][1] = map.get(nums[i]).remove();
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int n : pq) {
+            freq.merge(n, 1, Integer::sum);
         }
-        Arrays.sort(a,(x,y)->x[1]-y[1]);  //sort on basis of index
-        int ans[] = new int[k];
-        for(int i=0;i<k;i++){
-            ans[i] = a[i][0];
+        int[] seq = new int[k]; 
+        int i = 0;
+        for (int n : nums) {
+            if (freq.merge(n, -1, Integer::sum) >= 0) {
+                seq[i++] = n;
+            }
         }
-        return ans;
-    }
+        return seq;}
 }
