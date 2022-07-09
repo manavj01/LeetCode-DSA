42341fe627112fd286f291e4f9e3a1493e7023bd
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-09 17:11:28 +0530

    Time: 21 ms (8.80%), Space: 44.7 MB (61.97%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -1,22 +1,15 @@
 class Solution {
-    public int[] maxSubsequence(int[] nums, int k) {
-         PriorityQueue<Integer> pq = new PriorityQueue<>(k + 1);
-        for (int n : nums) {
-            pq.offer(n);
+    
+        public int[] maxSubsequence(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>
+                    (Comparator.comparingInt(i -> nums[i]));
+        for (int i = 0; i < nums.length; ++i) {
+            pq.offer(i);
             if (pq.size() > k) {
                 pq.poll();
             }
         }
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int n : pq) {
-            freq.merge(n, 1, Integer::sum);
-        }
-        int[] seq = new int[k]; 
-        int i = 0;
-        for (int n : nums) {
-            if (freq.merge(n, -1, Integer::sum) >= 0) {
-                seq[i++] = n;
-            }
-        }
-        return seq;}
+        return pq.stream().sorted().mapToInt(i -> nums[i]).toArray();
+    }
+    
 }
