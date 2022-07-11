349eeef8a50eca8aaaf7e2a0be7cf1961f8cc02a
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-11 15:48:39 +0530

    Time: 71 ms (49.95%), Space: 55.1 MB (96.86%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,31 +1,34 @@
 class Solution {
     public int longestConsecutive(int[] A) {
-        if(A.length == 0) return 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int c = 1;
-        for (int a : A) {
-            pq.offer(a);
-        }
+ 
+    if(A.length == 0) return 0; //edge case
+  
+  PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        System.out.println(pq);
-//        while (pq.size()>0) System.out.println(pq.poll());
-
-        int a = pq.poll();
-        int max= Integer.MIN_VALUE;
-        while (pq.size()>0) {
-            int b = pq.poll();
-            if (a == b) {
-                a = b;
-            } else if (a + 1 == b){
-                c++;
-                a = b;
-            max = Math.max(max,c);
-            }else if (a +1 != b){
-                c=1;
-                a=b;
-            }
-        }
-
-        return max== Integer.MIN_VALUE?1:max;
+int c = 1; // LCS Counter, set to 1 because a single digit is also consecutive.
+ 
+ for (int a : A) {
+        pq.offer(a);
     }
+
+    int a = pq.poll();
+    int max= Integer.MIN_VALUE;
+	
+    while (pq.size()>0) {
+	
+        int b = pq.poll();
+        if (a == b) { // if a==b then we just move forward and update variables
+            a = b;
+        } else if (a + 1 == b){ // this is when we inc. LCS counter
+            c++;
+            a = b;
+            max = Math.max(max,c); // we take the maximum LCS length and return it(cause there might be multiple)
+        }else if (a +1 != b){ // if variables are not equal, refresh counter,and start again
+            c=1;
+            a=b;
+        }
+    }
+
+    return max == Integer.MIN_VALUE ? 1 : max;
+}
 }
