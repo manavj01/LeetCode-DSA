de203683440063943a361be702e074d0c6c2a541
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-16 19:33:47 +0530

    Time: 19 ms (6.36%), Space: 43.7 MB (5.25%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        
+        int red0 =0;
+        int white1=0;
+        int blue2 = 0;
+        
+        for(int i =0; i < arr.length; i++ ){
+            if(arr[i] == 0){
+                red0++;
+            }else if(arr[i] == 1){
+                white1++;
+            }else if(arr[i] == 2){
+                blue2++;
+            } 
+            
+        }
+        int idx=0;
+        
+        while(red0 != 0){
+            arr[idx++] = 0;
+            red0--;
+        }
+         while(white1 != 0){
+            arr[idx++] = 1;
+            white1--;
+        }
+         while(blue2 != 0){
+            arr[idx++] = 2;
+            blue2--;
+        }
+        
+        System.out.println(red0+","+white1+","+blue2);
+    }
+}
