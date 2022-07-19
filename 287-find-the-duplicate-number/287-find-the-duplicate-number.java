9bb1bed107160ed37e8107cdb3fdff3b55a3e4db
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-19 22:07:50 +0530

    Time: 4 ms (95.11%), Space: 76.7 MB (22.85%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int[] freq =  new int[arr.length];
+        for (int a : arr){
+            freq[a]++;
+        }
+        int ret=0;
+        for (int i=0; i < freq.length;i++){
+            if (freq[i]>1){
+                ret= i;
+                break;
+            }
+        }
+        return ret;
+    }
+}
