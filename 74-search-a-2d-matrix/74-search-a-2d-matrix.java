3ab11f6f76a0e270bfaa1c64eb05174610c1c396
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-20 17:22:19 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (66.42%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+         for (int i=arr.length-1; i >= 0; i--){
+            if (target >= arr[i][0]){
+                for (int j=0; j<arr[0].length;j++){
+                    if (target==arr[i][j]){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
