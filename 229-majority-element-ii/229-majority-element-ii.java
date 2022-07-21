0673eda6bab6484458d9cbd40c128758a002805d
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-21 23:12:11 +0530

    Time: 5 ms (50.55%), Space: 48.8 MB (82.52%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,27 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-       Arrays.sort(nums);
-        int maj = nums.length / 3;
-        int c = 1;
-        int element = nums[0];
+       int maj = nums.length / 3;
+        int num1 = -1;
+        int num2 = -2;
+        int c1 = 0;
+        int c2 = 0;
 
-        List<Integer> list = new LinkedList<>();
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == element) {
-                c++;
+        for (int el : nums) {
+            if (el == num1) {
+                c1++;
+            } else if (el == num2) {
+                c2++;
+            } else if (c1 == 0) {
+                num1 = el;
+                c1 = 1;
+            } else if (c2 == 0) {
+                num2 = el;
+                c2 = 1;
             } else {
-                if (c > maj) {
-                    list.add(element);
-                }
-                element = nums[i];
-                c = 1;
+                c1--;
+                c2--;
             }
+        }
 
+        List<Integer> ans = new ArrayList<>();
+        c1 = 0;
+        c2 = 0;
+        for (int num : nums) {
+            if (num == num1) {
+                c1++;
+            } else if (num == num2) {
+                c2++;
+            }
         }
-        if (c>maj){
-            list.add(element);
+        if (c1 > maj) {
+            ans.add(num1);
         }
-        return list;
+        if (c2 > maj) {
+            ans.add(num2);
+        }
+        return ans;
 
     }
+
+    
 }
