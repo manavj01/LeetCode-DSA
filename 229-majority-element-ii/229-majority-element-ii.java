edd071eb8e658c785d6467540aeff7180c80f1f7
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-21 22:29:20 +0530

    Time: 5 ms (50.55%), Space: 49.6 MB (65.01%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,23 +1,27 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+       Arrays.sort(nums);
         int maj = nums.length / 3;
+        int c = 1;
+        int element = nums[0];
 
-        HashMap<Integer, Integer> freq = new HashMap<>();
-
-        for (int num : nums) {
-            if (freq.containsKey(num)) {
-                freq.put(num, freq.get(num) + 1);
+        List<Integer> list = new LinkedList<>();
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == element) {
+                c++;
             } else {
-                freq.put(num, 1);
+                if (c > maj) {
+                    list.add(element);
+                }
+                element = nums[i];
+                c = 1;
             }
-        }
-        List<Integer> list = new ArrayList<Integer>();
 
-        for (int a : freq.keySet()) {
-            if (freq.get(a) > maj) {
-                list.add(a);
-            }
+        }
+        if (c>maj){
+            list.add(element);
         }
         return list;
+
     }
 }
