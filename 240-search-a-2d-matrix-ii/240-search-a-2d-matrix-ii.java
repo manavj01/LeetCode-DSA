0adf47449a85108f843cbb4be3e61c044be96070
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-21 16:25:03 +0530

    Time: 11 ms (50.70%), Space: 58.1 MB (25.76%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -6,7 +6,8 @@ class Solution {
         int i = 0;
         int j = m - 1;
 
-        while ((i >= 0 && i < n) && (j < m && j >= 0)) {
+        
+            while ( i < n && j >= 0) {
             if (arr[i][j] == target){
                 return true;
             } else if (arr[i][j] > target) {
