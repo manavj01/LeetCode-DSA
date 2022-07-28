0bb81065910697da5aad10ec7cce141a34375967
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-29 00:01:25 +0530

    Time: 22 ms (87.55%), Space: 63.9 MB (81.14%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,34 +1,54 @@
 class Solution {
-    public int longestConsecutive(int[] A) {
+//     public int longestConsecutive(int[] A) {
  
-    if(A.length == 0) return 0; //edge case
+//     if(A.length == 0) return 0; //edge case
   
-  PriorityQueue<Integer> pq = new PriorityQueue<>();
+//   PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-    int c = 1; // LCS Counter, set to 1 because a single digit is also consecutive.
+//     int c = 1; // LCS Counter, set to 1 because a single digit is also consecutive.
  
-     for (int a : A) {
-        pq.offer(a);
-    }
+//      for (int a : A) {
+//         pq.offer(a);
+//     }
 
-    int a = pq.poll();
-    int max= Integer.MIN_VALUE;
+//     int a = pq.poll();
+//     int max= Integer.MIN_VALUE;
 	
-    while (pq.size()>0) {
+//     while (pq.size()>0) {
 	
-        int b = pq.poll();
-        if (a == b) { // if a==b then we just move forward and update variables
-            a = b;
-        } else if (a + 1 == b){ // this is when we inc. LCS counter
-            c++;
-            a = b;
-            max = Math.max(max,c); // we take the maximum LCS length and return it(cause there might be multiple)
-        }else if (a +1 != b){ // if variables are not equal, refresh counter,and start again
-            c=1;
-            a=b;
+//         int b = pq.poll();
+//         if (a == b) { // if a==b then we just move forward and update variables
+//             a = b;
+//         } else if (a + 1 == b){ // this is when we inc. LCS counter
+//             c++;
+//             a = b;
+//             max = Math.max(max,c); // we take the maximum LCS length and return it(cause there might be multiple)
+//         }else if (a +1 != b){ // if variables are not equal, refresh counter,and start again
+//             c=1;
+//             a=b;
+//         }
+//     }
+
+//     return max == Integer.MIN_VALUE ? 1 : max;
+// }
+     public  int longestConsecutive(int[] A) {
+         if(A.length ==0) return 0;
+        Arrays.sort(A);
+        int c = 1;
+        int maxc = Integer.MIN_VALUE;
+        for (int i = 1; i < A.length; i++) {
+            int a = A[i - 1];
+            int b = A[i];
+            if (a == b){
+                continue;
+            } else if (a + 1 == b) {
+                c++;
+            } else {
+                c = 1;
+            }
+            maxc = Math.max(c, maxc);
         }
-    }
 
-    return max == Integer.MIN_VALUE ? 1 : max;
-}
+        return maxc == Integer.MIN_VALUE ? 1 : maxc;
+    }
 }
