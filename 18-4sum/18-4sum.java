25ce725c9ee1ce8896fd1d55803bc8d717f96865
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-30 00:24:14 +0530

    Time: 31 ms (51.49%), Space: 44.9 MB (45.08%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,44 +1,63 @@
 class Solution {
-    public  List<List<Integer>> fourSum(int[] nums, int target) {
- List<List<Integer>> lists = new ArrayList<>();
-        Arrays.sort(nums);
-        int n = nums.length;
-        for (int i = 0; i < n - 3; i++) {
-            for (int j = i + 1; j < n - 2; j++) {
-                for (int k = j + 1; k < n - 1; k++) {
-                    List<Integer> list = new ArrayList<>();
-                    long sch = (long)nums[i] + (long)nums[j] + (long)nums[k];
-                    long x = target-sch;
-                    // System.out.println(sch);
-                    if (binarySearch(nums, x, k + 1, n - 1)) {
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        list.add(target - (int)sch);
-                        if (!lists.contains(list)) {
-                            lists.add(list);
-                        }
+    public  List<List<Integer>> fourSum(int[] num, int target) {
+        
+         ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        if (num == null || num.length == 0)
+            return res;
+        
+        int n = num.length;
+    
+        Arrays.sort(num); 
+    
+        for (int i = 0; i < n; i++) {
+        
+            long target_3 = target - num[i];
+        
+            for (int j = i + 1; j < n; j++) {
+            
+                long target_2 = target_3 - num[j];
+            
+                int front = j + 1;
+                int back = n - 1;
+            
+                while(front < back) {
+                
+                    int two_sum = num[front] + num[back];
+                
+                    if (two_sum < target_2) front++;
+                
+                    else if (two_sum > target_2) back--;
+                
+                    else {
+                    
+                        List<Integer> quad = new ArrayList<>(); 
+                        quad.add(num[i]);
+                        quad.add(num[j]);
+                        quad.add(num[front]);
+                        quad.add(num[back]);
+                        res.add(quad);
+                    
+                        // Processing the duplicates of number 3
+                        while (front < back && num[front] == quad.get(2)) ++front;
+                    
+                        // Processing the duplicates of number 4
+                        while (front < back && num[back] == quad.get(3)) --back;
+                
                     }
                 }
+                
+                // Processing the duplicates of number 2
+                while(j + 1 < n && num[j + 1] == num[j]) ++j;
             }
+        
+            // Processing the duplicates of number 1
+            while (i + 1 < n && num[i + 1] == num[i]) ++i;
+        
         }
-        return lists;
-
-    }
-
-       public  boolean binarySearch(int[] nums, long target, int si, int ei) {
-        int n = nums.length;
-        for (int i = si; i < n; i++) {
-            int mid = si + (ei - si) / 2;
-            if (nums[i] == target) {
-                return true;
-            }
-            if (nums[i] > target) {
-                ei = mid - 1;
-            } else {
-                si = mid + 1;
-            }
-        }
-        return false;
+        
+    
+        return res;
+            
     }
 }
