878db721b84f0c36ff2f1d570e1d7ce930996097
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-07-30 17:32:37 +0530

    Time: 6 ms (68.23%), Space: 97.5 MB (50.71%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -1,26 +1,6 @@
 class Solution {
     public long zeroFilledSubarray(int[] arr) {
-//           long pc = 0;
-//         long zc = 0;
 
-//         for (int i = 0; i < arr.length; i++) {
-//             if (arr[i] == 0) {
-//                 pc++;
-//             } else if (arr[i] != 0) {
-//                 if (pc > 0) {
-//                     pc = pc * (pc + 1) / 2;
-//                     zc += pc;
-//                     pc = 0;
-//                 }
-//             }
-//         }
-
-//         if (pc > 0) {
-//             pc = pc * (pc + 1) / 2;
-//             zc += pc;
-
-//         }
-//         return zc;
         long pc =0 , zc=0; 
         for(int a : arr){
             if( a==0){
