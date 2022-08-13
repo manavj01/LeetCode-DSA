cee86a1e612a0f9f2d6b17c2392ca6f54989b7e8
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-13 22:06:19 +0530

    Time: 16 ms (5.42%), Space: 44 MB (7.40%) - LeetHub

--- 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.java
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.java
@@ -1,31 +1,35 @@
 class Solution {
-    public String[] uncommonFromSentences(String s1, String s2) {
-       HashMap<String,Integer> map1 = new HashMap<>();       
-        // HashMap<String,Integer> map2 = new HashMap<>();
-        
-     for(String s : s1.split(" ")){
-         if(!map1.containsKey(s)){
-             map1.put(s,1);
-         }else{
-             map1.put(s,map1.get(s)+1);
-         }
-     }
-      for(String s : s2.split(" ")){
-         if(!map1.containsKey(s)){
-             map1.put(s,1);
-         }else{
-             map1.put(s,map1.get(s)+1);
-         }
-     }  
-       ArrayList<String> res = new ArrayList<>();
-        for(String s : map1.keySet()){
-            if(map1.get(s) == 1){
-                res.add(s);
-            }
-        }
+    public String[] uncommonFromSentences(String A, String B) {
+//        HashMap<String,Integer> map1 = new HashMap<>();       
+       
+//      for(String s : s1.split(" ")){
+//          if(!map1.containsKey(s)){
+//              map1.put(s,1);
+//          }else{
+//              map1.put(s,map1.get(s)+1);
+//          }
+//      }
+//       for(String s : s2.split(" ")){
+//          if(!map1.containsKey(s)){
+//              map1.put(s,1);
+//          }else{
+//              map1.put(s,map1.get(s)+1);
+//          }
+//      }  
+//        ArrayList<String> res = new ArrayList<>();
+//         for(String s : map1.keySet()){
+//             if(map1.get(s) == 1){
+//                 res.add(s);
+//             }
+//         }
         
       
-        return res.toArray(new String[res.size()]);
+//         return res.toArray(new String[res.size()]);
+         Map<String, Integer> count = new HashMap<>();
+        for (String s : (A + " " + B).split("\\s")) { 
+             count.put(s, count.getOrDefault(s, 0) + 1); 
+        }
+        return count.entrySet().stream().filter(e -> e.getValue() == 1).map(e -> e.getKey()).toArray(String[]::new);
 
     }
 }
