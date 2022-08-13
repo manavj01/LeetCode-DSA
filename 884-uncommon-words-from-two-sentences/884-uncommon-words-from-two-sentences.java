0bcfe92b1a5528dee8c50912e5f5fa2b7abd92fe
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-13 22:09:49 +0530

    Time: 4 ms (63.94%), Space: 43 MB (23.38%) - LeetHub

--- 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.java
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.java
@@ -1,37 +1,31 @@
 class Solution {
-    public String[] uncommonFromSentences(String A, String B) {
-//        HashMap<String,Integer> map1 = new HashMap<>();       
+    public String[] uncommonFromSentences(String s1, String s2) {
+       HashMap<String,Integer> map1 = new HashMap<>();       
        
-//      for(String s : s1.split(" ")){
-//          if(!map1.containsKey(s)){
-//              map1.put(s,1);
-//          }else{
-//              map1.put(s,map1.get(s)+1);
-//          }
-//      }
-//       for(String s : s2.split(" ")){
-//          if(!map1.containsKey(s)){
-//              map1.put(s,1);
-//          }else{
-//              map1.put(s,map1.get(s)+1);
-//          }
-//      }  
-//        ArrayList<String> res = new ArrayList<>();
-//         for(String s : map1.keySet()){
-//             if(map1.get(s) == 1){
-//                 res.add(s);
-//             }
-//         }
-        
-      
-//         return res.toArray(new String[res.size()]);
-         Set<String> distinct = new HashSet<>(), com = new HashSet<>();
-        for (String s : (A + " " + B).split("\\s")) {
-            if (com.contains(s) || !distinct.add(s)) { 
-                distinct.remove(s); com.add(s); 
+     for(String s : s1.split(" ")){
+         if(!map1.containsKey(s)){
+             map1.put(s,1);
+         }else{
+             map1.put(s,map1.get(s)+1);
+         }
+     }
+      for(String s : s2.split(" ")){
+         if(!map1.containsKey(s)){
+             map1.put(s,1);
+         }else{
+             map1.put(s,map1.get(s)+1);
+         }
+     }  
+       ArrayList<String> res = new ArrayList<>();
+        for(String s : map1.keySet()){
+            if(map1.get(s) == 1){
+                res.add(s);
             }
         }
-        return distinct.toArray(new String[distinct.size()]);
+        
+      
+        return res.toArray(new String[res.size()]);
+        
 
     }
 }
