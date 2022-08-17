0e30605515c230b66921821963f2d247d562196c
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-17 16:03:18 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (93.42%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -1,27 +1,10 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
        
-        int cNum = nums[0];
-        int nc = 0;
-
-        PriorityQueue<Integer> que = new PriorityQueue<>();
-
-        for (int num : nums){
-            if (cNum == num){
-                nc++;
-            }else {
-                cNum = num;
-                nc=1;
-            }
-            if (nc <3){
-            que.add(num);
-            }
-        }
-        System.out.println(que);
-        int size = que.size();
-        for (int j=0; j< size; j++){
-            nums[j] = que.remove();
-        }
-        return size;
+       int i = 0;
+    for (int n : nums)
+        if (i < 2 || n > nums[i-2])
+            nums[i++] = n;
+    return i;
     }
 }
