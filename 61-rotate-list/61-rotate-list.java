39eb042d95ca8e2f3e95fddc1b5aa3f1f9d67684
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-17 20:14:15 +0530

    Time: 1 ms (83.18%), Space: 42.9 MB (62.58%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -9,23 +9,33 @@
  * }
  */
 class Solution {
-    public ListNode rotateRight(ListNode head, int n ) {
-        if (head==null||head.next==null) return head;
-    ListNode dummy=new ListNode(0);
-    dummy.next=head;
-    ListNode fast=dummy,slow=dummy;
-
-    int i;
-    for (i=0;fast.next!=null;i++)//Get the total length 
-    	fast=fast.next;
-    
-    for (int j=i-n%i;j>0;j--) //Get the i-n%i th node
-    	slow=slow.next;
-    
-    fast.next=dummy.next; //Do the rotation
-    dummy.next=slow.next;
-    slow.next=null;
-    
-    return dummy.next;
+    public ListNode rotateRight(ListNode head, int k) {
+       
+        if(head == null) return null;
+        int size =0;
+        ListNode sizeTemp = head;
+        while(sizeTemp != null ){
+            sizeTemp = sizeTemp.next;
+            size++;
+        }
+        k =  k%size;
+        if(size == 1 ||  k==0) return head;
+        
+        ListNode tail = head;
+        while(tail.next != null){
+            tail = tail.next;
+        }
+        
+        ListNode newTail = head;
+        ListNode newHead = head;
+        for(int i=0; i< size-k-1; i++){
+            newTail = newTail.next;
+            newHead = newHead.next;
+        }
+        newHead = newHead.next;
+        newTail.next = null;
+        tail.next = head;
+        
+        return  newHead;
     }
 }
