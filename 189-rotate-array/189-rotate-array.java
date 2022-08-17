c028e28e3aa72ef67428b07e13bfe15b181953cb
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-17 20:51:04 +0530

    Time: 2 ms (48.99%), Space: 64.5 MB (48.10%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,7 +1,9 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-       k  = k % nums.length;
-        
+         k  = k % nums.length;
+         if(nums == null || nums.length < 2){
+        return;
+    }
         reverse(nums,0,nums.length-1);        
         reverse(nums,0,k-1);
         reverse(nums,k,nums.length-1);
