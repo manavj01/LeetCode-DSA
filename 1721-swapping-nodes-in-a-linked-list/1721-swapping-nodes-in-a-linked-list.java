e83fec01083d2beb4de8ba6cf7e9ada84d8eaa63
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-18 23:31:26 +0530

    Time: 4 ms (58.27%), Space: 57.4 MB (90.44%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,17 +10,62 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-       ListNode n1 = null, n2 = null;
-    for (var p = head; p != null; p = p.next) {
-        n2 = n2 == null ? null : n2.next;
-        if (--k == 0) {
-            n1 = p;
-            n2 = head;
-        }
+
+    if(head == null) {
+		return head;
+	}
+    
+    ListNode temp = head;
+    ListNode prev = null;
+	ListNode firstNode = null;
+	ListNode firstNodePrev = null;
+	ListNode secondNode = null;
+	ListNode secondNodePrev = null;
+	
+	int n = length(head) - k + 1;    	
+	int m = k;
+	while(temp != null) {
+		if(m-- > 0) {
+			firstNodePrev = prev;
+			firstNode = temp;
+		}
+		
+		if(n-- > 0) {
+			secondNodePrev = prev;
+			secondNode = temp;
+		}
+        prev = temp;
+		temp = temp.next;
+	}
+    
+    if(firstNodePrev != null) {
+        firstNodePrev.next = secondNode;
     }
-    var tmp = n1.val;
-    n1.val = n2.val;
-    n2.val = tmp;
-    return head;
+    else{
+        head = secondNode;
     }
+    
+    if(secondNodePrev != null) {
+        secondNodePrev.next = firstNode;
+    }
+    else{
+        head = firstNode;
+    }
+    
+    ListNode temp1=secondNode.next;
+    secondNode.next=firstNode.next;
+    firstNode.next=temp1;
+    return head;  	
+}
+
+public int length(ListNode head) {
+    ListNode temp = head;
+    int count = 0;
+    while (temp != null)
+    {
+        count++;
+        temp = temp.next;
+    }
+    return count;
+}
 }
