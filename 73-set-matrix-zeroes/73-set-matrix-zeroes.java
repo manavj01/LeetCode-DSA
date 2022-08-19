d45a65a6b51e3025dc44b49c69fd599590dd7866
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-19 22:35:13 +0530

    Time: 2 ms (47.20%), Space: 52.4 MB (84.90%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,21 +1,38 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-         int R = matrix.length; int C = matrix[0].length; 
-        Set<Integer> rows = new HashSet<Integer>(); 
-        Set<Integer> cols = new HashSet<Integer>(); 
-        // We mark the rows and columns that are to be made zero 
-    for (int i = 0; i < R; i++) { 
-        for (int j = 0; j < C; j++) { 
-            if (matrix[i][j] == 0) {
-                rows.add(i); cols.add(j); 
-            } 
-        } 
-    } 
-    // Iterate over the array once again and using the rows and cols sets, update the elements. 
-    for (int i = 0; i < R; i++) {
-        for (int j = 0; j < C; j++) {
-            if (rows.contains(i) || cols.contains(j)) {
-                matrix[i][j] = 0; 
-            } } }
+     	 
+    public class Pair{
+        int row ;
+        int col;
+        
+        Pair(int row, int col){
+            this.row=row;
+            this.col = col;
+        }
     }
+    public void setZeroes(int[][] matrix) {
+      int clm0 = 1;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) clm0 = 0;
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+                if (clm0 == 0) matrix[i][0] = 0;
+        }
+
+
+    }
+    
 }
