446f7ce639fe46b50837e68f8e80eb164db66fb5
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-19 19:37:31 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (91.99%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,20 +1,23 @@
 class Solution {
    public int heightChecker(int[] heights) {         
-        // perform a bucket-sort
-        int[] bucket = new int[101];
-        for(int number : heights) {
-            bucket[number]++;
-        }
-        
-        // check the ammount of disparities between the input array and the bucket
-        int count = 0, index = 0;
-        for(int i = 1; i <= 100; i++) {
-            while(bucket[i] > 0) {
-                if(i != heights[index++]) count++;
-                bucket[i]--;
-            }
-        }
-        return count;
-    }
+   
+       int[] freq = new int[101];
+       
+       for(int a : heights){
+           freq[a]++;
+       }
+       int idx =0;
+       int c =0;
+       for(int i=1; i<=100; i++){
+           while(freq[i]>0){
+               if(i != heights[idx]){
+                   c++;
+               }
+               freq[i]--;
+               idx++;
+           }
+       }
+       return c;
+   }
    
 }
