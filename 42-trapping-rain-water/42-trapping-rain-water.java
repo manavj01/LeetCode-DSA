ab1115d8659a2fbd06d3447921a074b489b4bdfb
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-24 21:26:47 +0530

    Time: 2 ms (59.62%), Space: 48.4 MB (73.12%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,25 +1,27 @@
 class Solution {
     public int trap(int[] height) {
-         int n = height.length;
-        int[] prefixMax = new int[n];
-        int[] suffixMax = new int[n];
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int res = 0;
+        int leftMax = 0, rightMax = 0;
 
-        int max = height[0];
-
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, height[i]);
-            prefixMax[i] = max;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    res += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    res += rightMax - height[right];
+                }
+                right--;
+            }
         }
-        max = height[n - 1];
-        for (int i = n - 1; i >= 0; i--) {
-            max = Math.max(max, height[i]);
-            suffixMax[i] = max;
-        }
-        int area =0;
-        for (int i=0; i<n; i++){
-            area += Math.min(prefixMax[i],suffixMax[i])-height[i];
-        }
-
-        return area;
+        return res;
     }
 }
