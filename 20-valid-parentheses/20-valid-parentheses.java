c3bca2725f38421df04b040ed6a7b545a7014942
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-25 13:02:35 +0530

    Time: 6 ms (8.49%), Space: 41.7 MB (75.66%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+       if(s.length() %2 == 1) return false;
+        
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)== '('){
+                st.push(')');
+            } else if( s.charAt(i) == '{'){
+                st.push('}');
+            } else if (s.charAt(i) == '['){
+                st.push(']');
+            }else if(st.isEmpty() || st.pop() != s.charAt(i)){
+                return false;
+            }
+        }
+        
+        return st.isEmpty();
+    }
+}
