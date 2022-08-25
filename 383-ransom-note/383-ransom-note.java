998701804f01d571d3ed5ebad17a47c21465e433
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-25 12:37:41 +0530

    Time: 4 ms (84.52%), Space: 46.1 MB (64.24%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] ransom = new int[126];
-        int[] magaz = new int[126];
+        int[] ransom = new int[26];
+        int[] magaz = new int[26];
         
         for(int i=0; i<ransomNote.length(); i++){
-            ransom[ransomNote.charAt(i)-'0']++;
+            ransom[ransomNote.charAt(i)-97]++;
         }
         for(int i=0; i<magazine.length(); i++){
-            magaz[magazine.charAt(i)-'0']++;
+            magaz[magazine.charAt(i)-97]++;
         }
         boolean flag = true;
-        for(int i=0; i<126; i++){
+        for(int i=0; i<26; i++){
             if(ransom[i] > magaz[i]){
                 flag = false;
                 break;
