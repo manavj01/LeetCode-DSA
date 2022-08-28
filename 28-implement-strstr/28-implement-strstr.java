572bc08fe2ca128af944de48dd10ca2845cf6f7a
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-28 22:59:41 +0530

    Time: 2 ms (12.48%), Space: 42.1 MB (39.15%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -2,7 +2,7 @@ class Solution {
     public int strStr(String haystack, String needle) {
       // if (needle.length() == 0) return 0;
         if (haystack.length() < needle.length()) return -1;
-        if (haystack.equals(needle)) return 0;
+        // if (haystack.equals(needle)) return 0;
         int i = 0;
         int j = 0;
         StringBuilder check = new StringBuilder();
