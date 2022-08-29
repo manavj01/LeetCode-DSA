bee819813d9dde01d0e741a419ad1e0842ba51a2
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-29 22:51:23 +0530

    Time: 18 ms (47.09%), Space: 53.7 MB (74.16%) - LeetHub

--- 561-array-partition/561-array-partition.java
+++ 561-array-partition/561-array-partition.java
@@ -1,11 +1,11 @@
 class Solution {
     public int arrayPairSum(int[] arr) {
-       
-        Arrays.sort(arr);
-        int msum =0;
-        for(int i =0 ; i< arr.length ; i+=2){
-            msum += arr[i]; 
+      Arrays.sort(arr);
+        int s =0;
+        for(int i=0; i<arr.length; i+=2){
+            s+= arr[i];
         }
-        return msum;
-        }
+        
+        return s;
+    }
 }
