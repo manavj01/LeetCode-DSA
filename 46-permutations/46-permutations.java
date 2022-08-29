cc922ddd0436bd7631faa81f04b1f81746c531a1
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-29 19:15:03 +0530

    Time: 3 ms (46.66%), Space: 44.6 MB (63.99%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        solve(nums, res, 0);
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        solve(nums, ds, res, freq);
         return res;
     }
 
-    public void solve(int[] nums, List<List<Integer>> res, int index) {
-        if (index == nums.length) {
-            List<Integer> ds = new ArrayList<>();
-            for (int num : nums) ds.add(num);
-            res.add(ds);
+    public void solve(int[] nums, List<Integer> ds, List<List<Integer>> res, boolean[] freq) {
+        if (ds.size() == nums.length) {
+            res.add(new ArrayList<>(ds));
+            return;
         }
 
-        for (int i = index; i < nums.length; i++) {
-            swap(nums, i, index);
-            solve(nums, res, index + 1);
-            swap(nums, i, index);
+        for (int i = 0; i < nums.length; i++) {
+            if (!freq[i]) {
+                freq[i] = true;
+                ds.add(nums[i]);
+                solve(nums, ds, res, freq);
+                ds.remove(ds.size()-1);
+                freq[i] = false;
+            }
         }
-    }
-
-    public static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
