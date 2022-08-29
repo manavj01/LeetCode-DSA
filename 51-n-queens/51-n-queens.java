f0ed7f2e32965cbede197845e19c1d4145144bdf
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-29 22:01:53 +0530

    Time: 2 ms (98.39%), Space: 45.3 MB (58.00%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -3,51 +3,35 @@ class Solution {
         List<List<String>> res = new ArrayList<>();
         char[][] board = new char[n][n];
         for (char[] c : board) Arrays.fill(c, '.');
-        solve1(res, board, 0, n);
+        int[] left = new int[n];
+        int[] upperDiagonal = new int[2*n-1];        
+        int[] lowerDiagonal = new int[2*n-1];
+        solve(res, board,left,upperDiagonal, lowerDiagonal ,0, n);
         return res;
     }
-
-    public void solve1(List<List<String>> res, char[][] board, int col, int n) {
-        if (col == n) {
+    
+    public void solve(List<List<String>> res,char[][] board,int[] left,int[] upperDiagonal,int[] lowerDiagonal, int col, int n ){
+        if(col == n){
             res.add(construct(board));
             return;
         }
-
-        for (int row = 0; row < n; row++) {
-            if (isQueenSafe(row, col, n, board)) {
+        
+        for(int row =0; row <n ; ++row){
+            if(left[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[n-1 + col - row] == 0){
+                left[row] = 1;
+                lowerDiagonal[row+col] =1;
+                upperDiagonal[n-1 + col - row] = 1;
                 board[row][col] = 'Q';
-                solve1(res, board, col + 1, n);
+                solve(res,board,left,upperDiagonal,lowerDiagonal,col+1,  n);
                 board[row][col] = '.';
+                left[row] =0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1+col-row] =0;
             }
         }
     }
 
-    public boolean isQueenSafe(int row, int col, int n, char[][] board) {
-
-        int dRow = row;
-        int dCol = col;
-
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-        row = dRow;
-        col = dCol;
-        while (col >= 0) {
-            if (board[row][col] == 'Q') return false;
-
-            col--;
-        }
-//        row = dRow;
-        col = dCol;
-        while (row < n && col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
+  
 
     public List<String> construct(char[][] board) {
          List<String> res = new ArrayList<>();
