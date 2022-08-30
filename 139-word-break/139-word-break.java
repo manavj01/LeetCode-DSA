44a07ce990ed53b25097d4b86c4ee203d5c35def
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-30 19:02:47 +0530

    Time: 3 ms (94.03%), Space: 42.5 MB (86.44%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,23 +1,24 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> dict) {
-     if (s == null || s.length() == 0) return false;
-  
-  int n = s.length();
-  
-  // dp[i] represents whether s[0...i] can be formed by dict
-  boolean[] dp = new boolean[n];
-  
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j <= i; j++) {
-      String sub = s.substring(j, i + 1);
-      
-      if (dict.contains(sub) && (j == 0 || dp[j - 1])) {
-        dp[i] = true;
-        break;
-      }
-    }
-  }
-  
-  return dp[n - 1];
+    public boolean wordBreak(String s, List<String> wordDict) {
+    boolean [] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        int maxLength = 0;
+        for(String word : wordDict) 
+            maxLength = Math.max(maxLength,word.length());
+        
+        for (int i = 0;i <= s.length();i++) {
+            for (int j = i - 1;j >= 0;j--) {
+                
+                if(i - j > maxLength)
+                    continue;
+                
+                if(dp[j] && wordDict.contains(s.substring(j,i))) {
+                    dp[i] = true;
+                    break;
+                }
+                
+            }
+        }
+        return dp[s.length()];
     }
 }
