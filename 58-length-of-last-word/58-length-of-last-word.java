5bb8c2ca2c7ef37265e8f8feb23fc87ab314f586
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-30 20:00:34 +0530

    Time: 0 ms (100.00%), Space: 42 MB (68.52%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,22 +1,34 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int c =0;
-        boolean flag = false;
-        if(s.charAt(s.length()-1) == ' ') flag = true;
+//         int c =0;
+//         boolean flag = false;
+//         if(s.charAt(s.length()-1) == ' ') flag = true;
        
-        for(int i=s.length()-1 ; i>=0; i--){
-            char ch = s.charAt(i);
-            if(!flag && ch ==' ') break;
-            if(!flag){
-                if(ch != ' ') c++;
-            }else{
-                if(ch != ' '){
-                 c++;
-                flag = false;  
-                }else  continue;
-            }
-        }
+//         for(int i=s.length()-1 ; i>=0; i--){
+//             char ch = s.charAt(i);
+//             if(!flag && ch ==' ') break;
+//             if(!flag){
+//                 if(ch != ' ') c++;
+//             }else{
+//                 if(ch != ' '){
+//                  c++;
+//                 flag = false;  
+//                 }else  continue;
+//             }
+//         }
         
-        return c;
+//         return c;
+        int count=0;   
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' ')
+                count++;
+            else{
+                if(count>0)
+                    return count;
+            }
+            
+            
+        }
+        return count;
     }
 }
