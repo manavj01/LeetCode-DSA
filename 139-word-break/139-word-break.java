ea68894f01528c6dc39859d2028bf8ba7ec1cf44
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-08-30 19:00:43 +0530

    Time: 34 ms (5.24%), Space: 47.9 MB (14.71%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,19 +1,23 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> wordList) {
-    boolean[] T = new boolean[s.length() + 1];
-    Set<String> set = new HashSet<>();
-    for (String word : wordList) {
-        set.add(word);
+    public boolean wordBreak(String s, List<String> dict) {
+     if (s == null || s.length() == 0) return false;
+  
+  int n = s.length();
+  
+  // dp[i] represents whether s[0...i] can be formed by dict
+  boolean[] dp = new boolean[n];
+  
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j <= i; j++) {
+      String sub = s.substring(j, i + 1);
+      
+      if (dict.contains(sub) && (j == 0 || dp[j - 1])) {
+        dp[i] = true;
+        break;
+      }
     }
-    T[0] = true;
-    for (int i = 1; i <= s.length(); i++) {
-        for (int j = 0; j < i; j++) {
-            if(T[j] && set.contains(s.substring(j, i))) {
-                T[i] = true;
-                break;
-            }
-        }
-    }
-    return T[s.length()];
+  }
+  
+  return dp[n - 1];
     }
 }
