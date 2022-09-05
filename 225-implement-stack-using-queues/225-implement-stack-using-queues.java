6ce4f5c8822aab1c79313b4e8674ce9958f02f6c
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-05 21:55:15 +0530

    Time: 1 ms (34.19%), Space: 42.1 MB (19.28%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,47 +1,40 @@
 class MyStack {
-     Queue<Integer> stack1;
-        Queue<Integer> stack2;
-
-        public MyStack() {
-            stack1 = new LinkedList<>();
-            stack2 = new LinkedList<>();
+    Queue<Integer> stack;
+    public MyStack() {
+        stack = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        stack.add(x);
+    }
+    
+    public int pop() {
+        int size = stack.size();
+        for(int i=1; i<size; i++){
+            int a =stack.poll();
+            stack.add(a);
         }
-
-        public void push(int x) {
-            stack1.add(x);
+        int ret = stack.poll();
+        // stack.add(ret);
+        return ret;
+    }
+    
+    public int top() {
+          int size = stack.size();
+        for(int i=1; i<size; i++){
+            int a =stack.poll();
+            stack.add(a);
         }
-
-        public int pop() {
-            while(stack1.size() != 1){
-                stack2.add(stack1.poll());
-            }
-            int ret = stack1.poll();
-            for(int i=stack2.size()-1; i>=0; i--){
-                stack1.add(stack2.poll());
-            }
-            return ret;
-        }
-
-        public int top() {
-//            int len = stack1.size();
-            for(int i=stack1.size()-1; i>0; i--){
-                stack2.add(stack1.poll());
-            }
-            int ret = stack1.peek();
-            stack2.add(stack1.poll());
-            for(int i=stack2.size()-1; i>=0; i--){
-                stack1.add(stack2.poll());
-            }
-            return ret;
-        }
-
-        public boolean empty() {
-            if(stack1.size() != 0){
-                return false;
-            }
-            return true;
-        }
+        int ret = stack.poll();
+        stack.add(ret);
+        return ret;
+    }
+    
+    public boolean empty() {
+         return stack.size() == 0;
+    }
 }
+
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack obj = new MyStack();
