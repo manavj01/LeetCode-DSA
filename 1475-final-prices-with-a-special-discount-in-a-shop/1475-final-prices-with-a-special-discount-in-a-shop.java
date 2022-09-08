a6311a8bc21c0c3c10e58cb44ceab6fd196a844a
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-08 20:31:30 +0530

    Time: 7 ms (18.49%), Space: 44.7 MB (23.39%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,26 +1,33 @@
 class Solution {
-    public int[] finalPrices(int[] prices) {
-        Stack<Integer> stack = new Stack<>();
-        int n = prices.length;
-        stack.push(prices[n-1]);
+    public int[] finalPrices(int[] A) {
+//         Stack<Integer> stack = new Stack<>();
+//         int n = prices.length;
+//         stack.push(prices[n-1]);
         
-        for(int i= n-2; i>=0; i--){
+//         for(int i= n-2; i>=0; i--){
             
-            int val = prices[i];
-            if(stack.peek()<=val){
-                prices[i] = val-stack.peek();
-            }else{
-                while(!stack.isEmpty() && stack.peek() > val){
-                    stack.pop();
-                }
-                if(!stack.isEmpty()){
-                    prices[i] =val- stack.peek();
-                }
-            }
+//             int val = prices[i];
+//             if(stack.peek()<=val){
+//                 prices[i] = val-stack.peek();
+//             }else{
+//                 while(!stack.isEmpty() && stack.peek() > val){
+//                     stack.pop();
+//                 }
+//                 if(!stack.isEmpty()){
+//                     prices[i] =val- stack.peek();
+//                 }
+//             }
             
-            stack.push(val);
+//             stack.push(val);
+//         }
+        
+//         return prices;
+         Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < A.length; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] >= A[i])
+                A[stack.pop()] -= A[i];
+            stack.push(i);
         }
-        
-        return prices;
+        return A;
     }
 }
