e6fc48b8c81fe52c854111ff62dcb97077ee1b35
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-08 22:01:35 +0530

    Time: 18 ms (36.64%), Space: 43.4 MB (70.50%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,48 +1,33 @@
 class Solution {
-   public String reverseWords(String s) {
-    if (s == null) return null;
-    
-    char[] a = s.toCharArray();
-    int n = a.length;
-    
-    // step 1. reverse the whole string
-    reverse(a, 0, n - 1);
-    // step 2. reverse each word
-    reverseWords(a, n);
-    // step 3. clean up spaces
-    return cleanSpaces(a, n);
-  }
-  
-  void reverseWords(char[] a, int n) {
-    int i = 0, j = 0;
-      
-    while (i < n) {
-      while (i < j || i < n && a[i] == ' ') i++; // skip spaces
-      while (j < i || j < n && a[j] != ' ') j++; // skip non spaces
-      reverse(a, i, j - 1);                      // reverse the word
+    public String reverseWords(String s) {
+       
+        // Stack<String> stack = new Stack<>();
+        // StringBuilder sb = new StringBuilder();
+        // s = s.trim();
+        // for(int i =0; i<s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     if(s.charAt(i) == ' ' && s.charAt(i-1) == ' ') continue;
+        //     if(ch == ' '){
+        //         stack.push(sb.toString());
+        //         // stack.push(" ");
+        //         sb = new StringBuilder();
+        //     }else{
+        //          sb.append(ch);
+        //     }
+        // }
+        // stack.push(sb.toString());
+        // sb = new StringBuilder();
+        // int size = stack.size();
+        // for(int i=0; i<size; i++){
+        //     if(i== size-1){
+        //     sb.append(stack.pop());
+        //         break;
+        //     }
+        //     sb.append(stack.pop()).append(" ");
+        // }
+        // return sb.toString();
+    String[] words = s.trim().split(" +");
+    Collections.reverse(Arrays.asList(words));
+    return String.join(" ", words);
     }
-  }
-  
-  // trim leading, trailing and multiple spaces
-  String cleanSpaces(char[] a, int n) {
-    int i = 0, j = 0;
-      
-    while (j < n) {
-      while (j < n && a[j] == ' ') j++;             // skip spaces
-      while (j < n && a[j] != ' ') a[i++] = a[j++]; // keep non spaces
-      while (j < n && a[j] == ' ') j++;             // skip spaces
-      if (j < n) a[i++] = ' ';                      // keep only one space
-    }
-  
-    return new String(a).substring(0, i);
-  }
-  
-  // reverse a[] from a[i] to a[j]
-  private void reverse(char[] a, int i, int j) {
-    while (i < j) {
-      char t = a[i];
-      a[i++] = a[j];
-      a[j--] = t;
-    }
-  }
 }
