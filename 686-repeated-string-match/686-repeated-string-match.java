ba1b338933942de86646bd3a6fd033cf34f7d1a0
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-10 17:40:04 +0530

    Time: 265 ms (66.92%), Space: 42.3 MB (92.48%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,18 +1,26 @@
 class Solution {
-     public static int repeatedStringMatch(String a, String b) {
-        StringBuilder str = new StringBuilder(a);
-        int c = 1;
-        int chck = -1;
-        while (str.length() <= b.length() + a.length()) {
-            chck = str.indexOf(b);
-            if (chck == -1) {
-                c++;
-                str.append(a);
-            } else {
-                return c;
-            }
+     public static int repeatedStringMatch(String A, String B) {
+        // StringBuilder str = new StringBuilder(a);
+        // int c = 1;
+        // int chck = -1;
+        // while (str.length() <= b.length() + a.length()) {
+        //     chck = str.indexOf(b);
+        //     if (chck == -1) {
+        //         c++;
+        //         str.append(a);
+        //     } else {
+        //         return c;
+        //     }
+        // }
+        // if(str.indexOf(b) != -1) return c;
+        // return -1;
+         StringBuilder sb = new StringBuilder();
+        int count=0;
+        while(sb.length()<=B.length()+A.length()){
+            count++;
+            sb.append(A);
+            if(sb.indexOf(B) != -1){return count;}
         }
-        if(str.indexOf(b) != -1) return c;
         return -1;
     }
 }
