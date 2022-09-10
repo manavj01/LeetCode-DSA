232c837b812bcaabf00e1d3205f209fd0d454058
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-11 01:21:27 +0530

    Time: 4 ms (99.95%), Space: 42 MB (94.02%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,10 +1,24 @@
 class Solution {
 
+    // public String intToRoman(int num) {
+    //     String M[] = {"", "M", "MM", "MMM"};
+    // String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    // String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    // String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    // return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    // }
     public String intToRoman(int num) {
-        String M[] = {"", "M", "MM", "MMM"};
-    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+        int[] vals = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < vals.length; i++) {
+            while (num >= vals[i]) {
+                res.append(roman[i]);
+                num = num - vals[i];
+            }
+        }
+        return res.toString();
     }
 }
