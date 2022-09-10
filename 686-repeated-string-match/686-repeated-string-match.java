dd7ddc781ef7a5a2fd5eaa72a90aa6ef927bd31b
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-10 17:43:31 +0530

    Time: 247 ms (75.47%), Space: 41.7 MB (98.46%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,26 +1,25 @@
 class Solution {
-     public static int repeatedStringMatch(String A, String B) {
-        // StringBuilder str = new StringBuilder(a);
-        // int c = 1;
+
+    public static int repeatedStringMatch(String A, String B) {
+        StringBuilder str = new StringBuilder();
+        int c = 0;
         // int chck = -1;
-        // while (str.length() <= b.length() + a.length()) {
-        //     chck = str.indexOf(b);
-        //     if (chck == -1) {
-        //         c++;
-        //         str.append(a);
-        //     } else {
-        //         return c;
-        //     }
-        // }
-        // if(str.indexOf(b) != -1) return c;
-        // return -1;
-         StringBuilder sb = new StringBuilder();
-        int count=0;
-        while(sb.length()<=B.length()+A.length()){
-            count++;
-            sb.append(A);
-            if(sb.indexOf(B) != -1){return count;}
+        while (str.length() <= A.length() + B.length()) {
+            c++;
+            str.append(A);
+            if (str.indexOf(B) != -1) {
+                return c;
+            }
         }
+        // if (str.indexOf(b) != -1) return c;
         return -1;
+        //  StringBuilder sb = new StringBuilder();
+        // int count=0;
+        // while(sb.length()<=B.length()+A.length()){
+        //     count++;
+        //     sb.append(A);
+        //     if(sb.indexOf(B) != -1){return count;}
+        // }
+        // return -1;
     }
 }
