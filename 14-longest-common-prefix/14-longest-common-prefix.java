b12c5bad646dd2d22a2d0226e4b3f52bf8e17b5e
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-10 17:03:23 +0530

    Time: 2 ms (50.49%), Space: 41.8 MB (72.00%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,35 +1,37 @@
 class Solution {
-
     public String longestCommonPrefix(String[] strs) {
-//         StringBuilder sb = new StringBuilder();
-
-//         int minLen = Integer.MAX_VALUE;
-//         int minIdx = -1;
-//         for (int i = 0; i < strs.length; i++) {
-//             if (strs[i].length() < minLen) {
-//                 minLen = strs[i].length();
-//                 minIdx = i;
-//             }
-//         }
-//         for (int i = 0; i < strs[minIdx].length(); i++) {
-//             char ch = strs[minIdx].charAt(i);
-//             for (int j = 0; j < strs.length; j++) {
-//                 if (ch != strs[j].charAt(i)) {
-//                     return sb.toString();
-//                 }
-//             }
-//             sb.append(ch);
-//         }
-//                 return sb.toString();
-
-//     }
-   if (strs.length == 0) return "";
-        StringBuilder prefix = new StringBuilder(strs[0]);
-        for (String str : strs) {
-            while (str.indexOf(String.valueOf(prefix)) != 0) {
-                prefix = new StringBuilder(prefix.substring(0, prefix.length() - 1));
+         StringBuilder sb = new StringBuilder();
+//        Arrays.sort(strs, Comparator.comparingInt(String::length));
+        // System.out.println(Arrays.toString(strs));
+        int minLen = Integer.MAX_VALUE;
+        int minIdx = -1;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() < minLen) {
+                minLen = strs[i].length();
+                minIdx = i;
             }
         }
-        return prefix.toString();
+        for (int i = 0; i < minLen; i++) {
+            char ch = strs[minIdx].charAt(i);
+            for (String str : strs) {
+                if (ch != str.charAt(i)) {
+                    return sb.toString();
+                }
+            }
+            sb.append(ch);
+        }
+
+        return sb.toString();
+
     }
+    //  public static String longestCommonPrefix(String[] strs) {
+    //     if (strs.length == 0) return "";
+    //     String prefix = strs[0];
+    //     for (String str : strs) {
+    //         while (str.indexOf(prefix) != 0) {
+    //             prefix = prefix.substring(0, prefix.length() - 1);
+    //         }
+    //     }
+    //     return prefix;
+    // }
 }
