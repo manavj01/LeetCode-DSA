767bd0521124704ea4b91482dd57fc996f5ba0f6
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-10 23:18:22 +0530

    Time: 2 ms (90.63%), Space: 41.9 MB (84.59%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,25 +1,22 @@
 class Solution {
    public int maxProfit(int k, int[] prices) {
-        int len = prices.length;
-        if (k >= len / 2) return quickSolve(prices);
-        
-        int[][] t = new int[k + 1][len];
-        for (int i = 1; i <= k; i++) {
-            int tmpMax =  -prices[0];
-            for (int j = 1; j < len; j++) {
-                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
-                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
-            }
-        }
-        return t[k][len - 1];
+        return DP(prices, k);
     }
     
-
-    private int quickSolve(int[] prices) {
-        int len = prices.length, profit = 0;
-        for (int i = 1; i < len; i++)
-            // as long as there is a price gap, we gain a profit.
-            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
-        return profit;
+    private int DP(int[] prices, int k) {
+        int[] costs = new int[k+1];
+        int[] profits = new int[k+1];
+        //costs[0] = prices[0];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        
+        for (int currentPrice : prices) {
+            for (int i = 0; i < k; i++) {
+                costs[i+1] = Math.min(costs[i+1], currentPrice - profits[i]);
+                profits[i+1] = Math.max(profits[i+1], currentPrice - costs[i+1]);
+            }
+        }
+        
+        return profits[k];
+            
     }
 }
