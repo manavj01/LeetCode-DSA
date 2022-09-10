4c5df55655be8d94e27a7edc5a0cb4f098eb82ad
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-10 21:54:34 +0530

    Time: 128 ms (28.23%), Space: 53.7 MB (47.03%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -1,6 +1,15 @@
 class Solution {
     public boolean repeatedSubstringPattern(String str) {
-         String s = str + str;
-        return s.substring(1, s.length() - 1).contains(str);
+         StringBuilder sb = new StringBuilder(str).append(str);
+        System.out.println(sb); 
+        sb.delete(sb.length() - 1, sb.length());
+        // Removing the first character
+        // of a string
+                System.out.println(sb); 
+
+        sb.delete(0, 1);
+                System.out.println(sb); 
+
+        return sb.indexOf(str) == -1 ? false : true;
     }
 }
