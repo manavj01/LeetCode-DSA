dd085bf5f300dc82fbc5eda46b12838f6e5c5729
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-12 20:37:24 +0530

    Time: 771 ms (9.44%), Space: 292.8 MB (5.94%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -13,14 +13,10 @@ class Solution {
                 } else if (g == 1) {
                     if (s.charAt(i) == s.charAt(j)) {
                         dp[i][j] = true;
-                    } else {
-                        dp[i][j] = false;
                     }
                 } else {
                     if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] == true) {
                         dp[i][j] = true;
-                    } else {
-                        dp[i][j] = false;
                     }
                 }
                 if (dp[i][j]) {
