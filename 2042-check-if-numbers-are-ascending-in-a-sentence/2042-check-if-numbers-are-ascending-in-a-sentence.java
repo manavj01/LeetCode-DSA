a3f0158d5849738266a7ab9faf9b51399c59c200
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-12 13:53:30 +0530

    Time: 0 ms (100.00%), Space: 40 MB (99.14%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -1,19 +1,33 @@
 class Solution {
+
+    //     public boolean areNumbersAscending(String s) {
+    //         // Pattern pattern = Pattern.compile("[^0-9]");
+    //         String numberOnly = s.replaceAll("\\D+", " ");
+    //         String[] arr =numberOnly.split(" ");
+
+    //         for(int i=1; i<arr.length; i++){
+    //            if( arr[i-1] == "") continue;
+    //             int prev = Integer.parseInt(arr[i-1]);
+    //             int curr = Integer.parseInt(arr[i]);
+
+    //             if(prev >= curr){
+    //                 return false;
+    //             }
+    //         }
+    //         return true;
+    //     }
     public boolean areNumbersAscending(String s) {
-        // Pattern pattern = Pattern.compile("[^0-9]");
-        String numberOnly = s.replaceAll("\\D+", " ");
-        
-        String[] arr =numberOnly.split(" ");
-
-        for(int i=1; i<arr.length; i++){
-           if( arr[i-1] == "") continue;
-            int prev = Integer.parseInt(arr[i-1]);
-            int curr = Integer.parseInt(arr[i]);
-
-            if(prev >= curr){
-                return false;
+        int prev = 0, cur = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (s.charAt(i) >= 48 && s.charAt(i) <= 57) {
+                cur = cur * 10 + (ch - '0');
+            } else if (cur != 0) {
+                if (prev >= cur) return false;
+                prev = cur;
+                cur = 0;
             }
         }
-        return true;
+        return cur == 0 || prev < cur;
     }
 }
