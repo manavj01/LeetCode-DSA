4c2521a648cc0fb4185aefe0d65ca5df55010546
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-14 21:40:53 +0530

    Time: 2 ms (99.89%), Space: 42.3 MB (93.17%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -14,58 +14,65 @@
  * }
  */
 class Solution {
-
-   class Pair{
+public  class vPair {
         TreeNode node;
-        int x;  //horizontal
-        int y;  //depth
-        Pair(TreeNode n, int x, int y)
-        {
-            node = n;
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> lists = new ArrayList<>();
-        Map<Integer, List<Pair>> map = new HashMap<>(); //x -> list (some nodes might have same y in the list)
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root, 0, 0));
-        int min = 0, max = 0;
-        while(!q.isEmpty())
-        {
-            Pair p = q.remove(); 
-            
-            min = Math.min(min, p.x);
-            max = Math.max(max, p.x);
-            
-            if(!map.containsKey(p.x)) 
-                map.put(p.x, new ArrayList<>());
-            map.get(p.x).add(p);
-            
-            if(p.node.left!=null) q.add(new Pair(p.node.left, p.x-1, p.y+1));
-            if(p.node.right!=null) q.add(new Pair(p.node.right, p.x+1, p.y+1));
-        }        
+        int hl;
+        int vl;
 
-        for(int i=min; i<=max; i++)
-        {
-            Collections.sort(map.get(i), new Comparator<Pair>(){
-                public int compare(Pair a, Pair b)
-                {
-                    if(a.y==b.y) //when y is equal, sort it by value
-                        return a.node.val - b.node.val;
-                    return 0; //otherwise don't change the order as BFS ganrantees that top nodes are visited first
-                }
-            });
-            List<Integer> list = new ArrayList<>();
-            for(int j=0; j<map.get(i).size(); j++)
-            {
-                list.add(map.get(i).get(j).node.val);
-            }
-            lists.add(list);
+        vPair(TreeNode node, int hl, int vl) {
+            this.node = node;
+            this.hl = hl;
+            this.vl = vl;
         }
-        return lists;   
+
+    }
+
+
+    public  List<List<Integer>> verticalTraversal(TreeNode root) {
+          Queue<vPair> que = new PriorityQueue<>(new Comparator<vPair>() {
+            @Override
+            public int compare(vPair o1, vPair o2) {
+                if (o1.vl < o2.vl){
+                    return -1;
+                }else if (o1.vl > o2.vl){
+                    return 1;
+                } else  {
+                    return Integer.compare(o1.node.val , o2.node.val);
+                }
+            }
+        });
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int minhl = 0, maxhl = 0;
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        que.add(new vPair(root, 0,0));
+
+        while (que.size() != 0) {
+            int size = que.size();
+
+            while (size-- > 0) {
+                vPair rp = que.poll();
+
+                maxhl = Math.max(maxhl, rp.hl);
+                minhl = Math.min(minhl, rp.hl);
+
+                map.putIfAbsent(rp.hl, new ArrayList<>());
+                map.get(rp.hl).add(rp.node.val);
+
+                if (rp.node.left != null) {
+                    que.add(new vPair(rp.node.left, rp.hl - 1, rp.vl+1));
+                }
+                if (rp.node.right != null) {
+                    que.add(new vPair(rp.node.right, rp.hl + 1,rp.vl+1));
+                }
+            }
+        }
+        for (int i = minhl; i <= maxhl; i++) {
+            // Collections.sort(map.get(i));
+            ans.add(map.get(i));
+        }
+        return ans;
     }
 }
