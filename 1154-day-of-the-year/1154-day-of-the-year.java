91b92e12ae19039b9f05810cf48c0ed51b20bb12
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-18 01:57:17 +0530

    Time: 65 ms (9.66%), Space: 54.2 MB (29.54%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.java
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -1,9 +1,10 @@
 class Solution {
     public int dayOfYear(String date) {
         int[] months = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-        int year = Integer.parseInt(date.substring(0,4));
-        int month = Integer.parseInt(date.substring(5,7));
-        int day = Integer.parseInt(date.substring(8));
+        String[] str = date.split("-");
+        int year = Integer.parseInt(str[0]);
+        int month = Integer.parseInt(str[1]);
+        int day = Integer.parseInt(str[2]);
         int d = 0;
         if(((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)){
            if(month >2 )  d++;
