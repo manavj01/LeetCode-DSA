4f7617d7de15c20a068eb6f74e82b82545b15382
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-22 18:45:38 +0530

    Time: 1 ms (80.79%), Space: 43 MB (25.26%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -15,39 +15,44 @@
  */
 class Solution {
 
-    //     TreeNode prev = null;
-    //     public void flatten(TreeNode root) {
-    //             if(root == null) return ;
-
-    //         flatten(root.right);
-    //         flatten(root.left);
-
-    //         root.left = null;
-    //         root.right = prev;
-
-    //         prev = root;
-
-    //     }
-    public void flatten(TreeNode root) {
-        if (root == null) return;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        TreeNode prev = null;
-
-        while (!st.isEmpty()) {
-            TreeNode cur = st.peek();
-            st.pop();
-
-            if (cur.right != null) {
-                st.push(cur.right);
-            }
-            if (cur.left != null) {
-                st.push(cur.left);
-            }
-            if (!st.isEmpty()) {
-                cur.right = st.peek();
-            }
-            cur.left = null;
+        // TreeNode prev = null;
+        public void flatten(TreeNode root) {
+               TreeNode[] arr = new TreeNode[1];
+            arr[0] = null;
+            solve(root,arr);
         }
+    public void solve(TreeNode root ,TreeNode[] arr){
+         if(root == null) return ;
+
+            solve(root.right,arr);
+            solve(root.left,arr);
+
+            root.left = null;
+            root.right = arr[0];
+
+            arr[0] = root;
+
     }
+//     public void flatten(TreeNode root) {
+//         if (root == null) return;
+//         Stack<TreeNode> st = new Stack<>();
+//         st.push(root);
+//         TreeNode prev = null;
+
+//         while (!st.isEmpty()) {
+//             TreeNode cur = st.peek();
+//             st.pop();
+
+//             if (cur.right != null) {
+//                 st.push(cur.right);
+//             }
+//             if (cur.left != null) {
+//                 st.push(cur.left);
+//             }
+//             if (!st.isEmpty()) {
+//                 cur.right = st.peek();
+//             }
+//             cur.left = null;
+//         }
+//     }
 }
