aed3d984caa7b0abb5958a868130f3208a987cf9
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-23 11:21:23 +0530

    Time: 111 ms (18.27%), Space: 146.5 MB (65.17%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -3,7 +3,7 @@ class Solution {
       Deque<Integer> q = new ArrayDeque<>();  // stores *indices*
         int n = nums.length-k+1;
         List<Integer> res = new ArrayList<>(n);
-        // int[] result = new int[n-k+1];
+        int[] result = new int[n];
         int ri=0;
         for (int i = 0; i < nums.length; i++) {
             while (!q.isEmpty() && nums[q.getLast()] <= nums[i]) {
@@ -17,8 +17,10 @@ class Solution {
             // if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
             if (i >= k - 1) {
                 res.add(nums[q.peek()]);
+                result[ri++] = nums[q.peek()];
             }
         }
-        return res.stream().mapToInt(i->i).toArray();            
+        return result;
+        // return res.stream().mapToInt(i->i).toArray();            
     }
 }
