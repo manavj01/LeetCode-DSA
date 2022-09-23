46e6b75e64f197fd86a6b626bcbab1eee2caeb22
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-23 11:21:48 +0530

    Time: 65 ms (52.74%), Space: 155.2 MB (20.45%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
       Deque<Integer> q = new ArrayDeque<>();  // stores *indices*
         int n = nums.length-k+1;
-        List<Integer> res = new ArrayList<>(n);
+        // List<Integer> res = new ArrayList<>(n);
         int[] result = new int[n];
         int ri=0;
         for (int i = 0; i < nums.length; i++) {
@@ -16,7 +16,7 @@ class Solution {
             }
             // if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
             if (i >= k - 1) {
-                res.add(nums[q.peek()]);
+                // res.add(nums[q.peek()]);
                 result[ri++] = nums[q.peek()];
             }
         }
