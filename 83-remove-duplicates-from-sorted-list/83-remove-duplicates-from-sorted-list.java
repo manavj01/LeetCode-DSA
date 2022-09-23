6eee208a23b2dc7bab116124f4c7a45f498d8a9e
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-24 00:42:33 +0530

    Time: 1 ms (80.72%), Space: 44.3 MB (35.15%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -16,14 +16,12 @@ class Solution {
         
         while(temp != null && temp.next != null){
             if(temp.next.val == temp.val){
-                ListNode trav = temp;
-                while(trav != null && trav.val == temp.val ){
-                    trav = trav.next;
-                }
-            temp.next = trav;
-            }
-            
-            temp = temp.next;
+                // ListNode trav = temp;
+                // while(trav != null && trav.val == temp.val ){
+                //     trav = trav.next;
+                // }
+            temp.next = temp.next.next;
+            }else temp = temp.next;
         }
         
         return head;
