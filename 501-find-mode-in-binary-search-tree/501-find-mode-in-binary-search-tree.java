f69a0f824db49c4860c6fc80f0e0398dee5b14ad
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-25 17:09:14 +0530

    Time: 27 ms (5.12%), Space: 47.8 MB (14.05%) - LeetHub

---
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int[] findMode(TreeNode root) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        solve(root,map);
+        
+        int maxKey=0;
+        int maxVal = 0;
+        
+        for(int key : map.keySet()){
+            if(map.get(key) > maxVal){
+                maxVal = map.get(key);
+                maxKey = key;
+            }
+        }
+        
+        for(int key : map.keySet()){
+            if(maxVal == map.get(key)){
+                arr.add(key);
+            }
+        }
+      
+        
+        return arr.stream().mapToInt(i -> i).toArray();
+    }
+    public void solve(TreeNode root , HashMap<Integer,Integer> frequencies){
+        if(root == null) return;
+        
+        frequencies.put(root.val, frequencies.getOrDefault(root.val, 0) + 1);
+        solve(root.left,frequencies);
+        solve(root.right,frequencies);
+    }
+}
