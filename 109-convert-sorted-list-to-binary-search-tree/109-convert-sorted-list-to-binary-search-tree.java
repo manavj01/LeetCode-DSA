3a0e2403cdb1b2b6b67bb51b1aa041d4e134cd41
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-25 14:54:07 +0530

    Time: 1 ms (90.80%), Space: 46.7 MB (78.18%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
@@ -24,34 +24,62 @@
  * }
  */
 class Solution {
-    public TreeNode sortedListToBST(ListNode head) {
-        if(head == null) return null;
-        ArrayList<ListNode> arr = new ArrayList<>();
-        ListNode temp = head;
-        
-        while(temp != null){
-            arr.add(temp);
-            temp = temp.next;
-        }
-        int lo = 0;
-        int hi =arr.size()-1;
-        
-        TreeNode root = solve(arr,lo,hi);
-        return root;
-    }
+     public TreeNode sortedListToBST(ListNode head) {
+         if(head == null) return null;
+         if(head.next == null) return new TreeNode(head.val);
+         
+         ListNode slow=head,fast = head;
+         ListNode leftEnd = slow;
+         ListNode rightHead ;
+         while(fast!= null && fast.next != null){
+             leftEnd = slow;
+             slow = slow.next;             
+             fast = fast.next.next;
+         }
+         
+         TreeNode mid = new TreeNode(slow.val);
+         rightHead = slow.next;
+         leftEnd.next = null;
+         
+        if(slow != head) mid.left = sortedListToBST(head);
+         mid.right = sortedListToBST(rightHead);
+         
+         
+         return mid;
+         
+         
+     }
     
-    public TreeNode solve(ArrayList<ListNode> arr, int lo, int hi){
-        if(lo == hi){
-            return new TreeNode(arr.get(lo).val);
-        }
-        if(lo > hi) return null;
-        int mid = (lo+hi)/2;
+    
+    // approach 1
+//     public TreeNode sortedListToBST(ListNode head) {
+//         if(head == null) return null;
+//         ArrayList<ListNode> arr = new ArrayList<>();
+//         ListNode temp = head;
         
-        TreeNode root = new TreeNode(arr.get(mid).val);
+//         while(temp != null){
+//             arr.add(temp);
+//             temp = temp.next;
+//         }
+//         int lo = 0;
+//         int hi =arr.size()-1;
         
-        root.left = solve(arr,lo,mid-1);
-        root.right = solve(arr,mid+1,hi);
+//         TreeNode root = solve(arr,lo,hi);
+//         return root;
+//     }
+    
+//     public TreeNode solve(ArrayList<ListNode> arr, int lo, int hi){
+//         if(lo == hi){
+//             return new TreeNode(arr.get(lo).val);
+//         }
+//         if(lo > hi) return null;
+//         int mid = (lo+hi)/2;
+        
+//         TreeNode root = new TreeNode(arr.get(mid).val);
+        
+//         root.left = solve(arr,lo,mid-1);
+//         root.right = solve(arr,mid+1,hi);
 
-        return root;
-    }
+//         return root;
+//     }
 }
