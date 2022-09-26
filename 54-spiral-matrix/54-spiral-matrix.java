7c4dc5048445715b96152c2f148db61794ab50ab
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-26 21:08:00 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (18.36%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,50 +1,44 @@
 class Solution {
+
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
-        
+
         int n = matrix.length;
         int m = matrix[0].length;
-        int tot = n*m;
-        int c=0;
-        int rMin =0;
-        int cMin =0;
-        int rMax = n-1;
-        int cMax = m-1;
-        while(c<tot){
-            if(cMin>cMax) break;
-            // if(rMin>rMax) break;
-            for(int j = cMin; j<=cMax; j++){
+        int tot = n * m;
+        int c = 0;
+        int rMin = 0;
+        int cMin = 0;
+        int rMax = n - 1;
+        int cMax = m - 1;
+        while (c < tot) {
+            if (cMin > cMax) break;
+            for (int j = cMin; j <= cMax; j++) {
                 res.add(matrix[rMin][j]);
                 c++;
             }
             rMin++;
-              // if(cMin>cMax) break;
-            if(rMin>rMax) break;
-            for(int i=rMin; i<=rMax; i++){
+            if (rMin > rMax) break;
+            for (int i = rMin; i <= rMax; i++) {
                 res.add(matrix[i][cMax]);
                 c++;
             }
             cMax--;
-              if(cMin>cMax) break;
-            // if(rMin>rMax) break;
-            for(int j=cMax; j>=cMin; j--){
+            if (cMin > cMax) break;
+            for (int j = cMax; j >= cMin; j--) {
                 res.add(matrix[rMax][j]);
                 c++;
             }
             rMax--;
-            // if(cMin>cMax) break;
-            if(rMin>rMax) break;
-            for(int i=rMax; i>=rMin; i--){
+            if (rMin > rMax) break;
+            for (int i = rMax; i >= rMin; i--) {
                 res.add(matrix[i][cMin]);
                 c++;
             }
-             
+
             cMin++;
-            // if(cMin>cMax) break;
-            // if(rMin>rMax) break;
         }
-        
-        
+
         return res;
     }
 }
