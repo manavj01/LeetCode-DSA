61e1b3bf22cebe8d609d657eebe309eae71ffb6f
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-27 21:36:16 +0530

    Time: 25 ms (69.24%), Space: 54.4 MB (52.72%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -1,29 +1,37 @@
 class Solution {
 
-    public String pushDominoes(String d) {
-       d = 'L' + d + 'R';
-        StringBuilder res = new StringBuilder();
-        for (int i = 0, j = 1; j < d.length(); ++j) {
-            if (d.charAt(j) == '.') continue;
-            int middle = j - i - 1;
-            if (i > 0)
-                res.append(d.charAt(i));
-            if (d.charAt(i) == d.charAt(j))
-                for (int k = 0; k < middle; k++)
-                    res.append(d.charAt(i));
-            else if (d.charAt(i) == 'L' && d.charAt(j) == 'R')
-                for (int k = 0; k < middle; k++)
-                    res.append('.');
-            else {
-                for (int k = 0; k < middle / 2; k++)
-                    res.append('R');
-                if (middle % 2 == 1)
-                    res.append('.');
-                for (int k = 0; k < middle / 2; k++)
-                    res.append('L');
+    public String pushDominoes(String dominoes) {
+      final int N = dominoes.length();
+        if (N <= 1) return dominoes;
+        char[] res = dominoes.toCharArray();
+        int i = 0;
+        while (i < N) {
+            if (res[i] == '.') {
+                i++;
+            } else if (res[i] == 'L') {  // push left
+                int j = i-1;
+                while (j >= 0 && res[j] == '.') {
+                    res[j--] = 'L';
+                }
+                i++;
+            } else { // res[i] == 'R'
+                int j = i+1;
+                while (j < N && res[j] == '.') { // try to find 'R' or 'L' in the right side
+                    j++;
+                }
+                if (j < N && res[j] == 'L') { // if found 'L', push left and right
+                    for (int l = i+1, r = j-1; l < r; l++, r--) {
+                        res[l] = 'R';
+                        res[r] = 'L';
+                    }
+                    i = j + 1;
+                } else {  // if no 'L', push right
+                    while (i < j) {
+                        res[i++] = 'R';
+                    }
+                }
             }
-            i = j;
         }
-        return res.toString();
+        return String.valueOf(res);
     }
 }
