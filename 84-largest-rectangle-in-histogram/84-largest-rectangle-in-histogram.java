1ade114d045f44d2a05f5d1766edb5cda8ed10bb
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-09-29 16:42:48 +0530

    Time: 326 ms (23.08%), Space: 99.7 MB (14.76%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,45 +1,38 @@
 class Solution {
     public int largestRectangleArea(int[] arr) {
        
-        int rb[] = new int[arr.length];  //nse index on the right
+        int n = arr.length;
+        int rb[] = new int[n];  //nse index on the right
         Stack<Integer> st = new Stack<>();
-        st.push(arr.length - 1);
-        rb[arr.length - 1] = arr.length;
+        st.push(n - 1);
+        rb[n - 1] = n;
 
         for (int i = arr.length - 2; i >= 0; i--) {
             while (st.size() > 0 && arr[i] <= arr[st.peek()]) {
                 st.pop();
             }
 
-            if (st.size() == 0) {
-                rb[i] = arr.length;
-            } else {
-                rb[i] = st.peek();
-            }
+            rb[i] = st.isEmpty() ? n : st.peek();
             st.push(i);
         }
 
-        int lb[] = new int[arr.length];  //nse index on the left
+        int lb[] = new int[n];  //nse index on the left
 
         st = new Stack<>();
         st.push(0);
         lb[0] = -1;
 
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < n; i++) {
             while (st.size() > 0 && arr[i] <= arr[st.peek()]) {
                 st.pop();
             }
-            if (st.size() == 0) {
-                lb[i] = -1;
-            } else {
-                lb[i] = st.peek();
-            }
+           
+            lb[i] = st.isEmpty() ? -1 : st.peek();
             st.push(i);
         }
-
+        
         int maxArea = 0;
-
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             int width = rb[i] - lb[i] - 1;
             int area = arr[i] * width;
             if (area > maxArea) {
