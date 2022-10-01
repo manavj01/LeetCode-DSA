656595045861c1f6466aa06d82a3b60b9040b16b
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-01 15:03:48 +0530

    Time: 1 ms (78.97%), Space: 42.7 MB (45.16%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -9,30 +9,33 @@ class Solution {
             if (arr[mid] == target) {
                 return mid;
             }
-
-            // if (nums[lo] <= nums[mid]) {
-            //     if (target >= nums[lo] && target <= nums[mid]) {
-            //         hi = mid - 1;
-            //     } else lo = mid + 1;
-            // } else {
-            //     if (target >= nums[mid] && target <= nums[hi]) {
-            //         lo = mid + 1;
-            //     } else hi = mid - 1;
-            // }
-     
-            if(arr[lo] <= arr[mid]){
-                if(target >= arr[lo] && target <= arr[mid]){
+            
+            if(arr[0] <= arr[mid]){
+                if(target >= arr[0] && target <= arr[mid]){
                     hi = mid-1;
-                }else {
+                }else{
                     lo = mid+1;
                 }
             }else{
-                if(target >= arr[mid] && target <= arr[hi] ){
+                if(target >= arr[mid] && target <=arr[arr.length-1]){
                     lo = mid+1;
                 }else{
                     hi = mid-1;
                 }
             }
+            // if(arr[lo] <= arr[mid]){
+            //     if(target >= arr[lo] && target <= arr[mid]){
+            //         hi = mid-1;
+            //     }else {
+            //         lo = mid+1;
+            //     }
+            // }else{
+            //     if(target >= arr[mid] && target <= arr[hi] ){
+            //         lo = mid+1;
+            //     }else{
+            //         hi = mid-1;
+            //     }
+            // }
         }
         return -1;
     }
