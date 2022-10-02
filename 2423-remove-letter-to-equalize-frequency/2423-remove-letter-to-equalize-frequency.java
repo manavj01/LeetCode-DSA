25bd991600ca81b283780d2279ca6f093b6c9a63
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-02 21:44:38 +0530

    Time: 9 ms (100.00%), Space: 42.3 MB (100.00%) - LeetHub

---
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean equalFrequency(String word) {
+        if(word.length() <=3) return true;
+        int[] fMap = new int[26];
+        
+        for(int i=0; i<word.length(); i++){
+         fMap[word.charAt(i)-'a']++;
+        }
+     
+        Arrays.sort(fMap);
+        if(fMap[24] ==0 ) return true;
+        boolean check = false;
+        for(int i =0; i<=25; i++){
+            if(fMap[i] == 0) continue;
+            fMap[i]--;
+            System.out.println(Arrays.toString(fMap));
+
+            for(int j=0; j<25; j++){
+                if(fMap[j] == 0) continue;
+                
+                if(fMap[j] == fMap[j+1]){
+                    check = true;
+                }else{
+                    check = false;
+                    break;
+                }
+            }
+            if(check) return true;
+            fMap[i]++;
+        }
+        
+        
+        
+        
+        return check;
+        
+    }
+}
