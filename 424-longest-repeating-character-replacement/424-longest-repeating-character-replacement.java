b55f90a2012db9dade89d351acd20dffa54469a3
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-04 22:38:20 +0530

    Time: 5 ms (96.09%), Space: 41.7 MB (99.62%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -4,13 +4,15 @@ class Solution {
         int maxLen = 0;
         int left=0;
         int right = 0;
-        int maxFreq = 0;
         int[] freq = new int[26];
+                    int maxFreq = 0;
 
-        for (left = 0, right = 0; right < s.length(); right++) {
+        for ( right = 0; right < s.length(); right++) {
             freq[s.charAt(right) - 'A']++;
-            
-            maxFreq = Math.max(maxFreq,freq[s.charAt(right)-'A']);
+
+            for(int i=0; i<26; i++){
+                maxFreq = Math.max(maxFreq,freq[i]);
+            }
 
             while ((right - left + 1) - maxFreq > k) {
                 freq[s.charAt(left) - 'A']--;
