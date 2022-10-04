ac8148aac03965ab32dcf9aff272309f1a106050
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-04 21:43:16 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (24.75%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
@@ -15,19 +15,34 @@
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-        solve(arr,new ArrayList<Integer>(), root);
+        // 1st approach
+//         ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+//         solve(arr,new ArrayList<Integer>(), root);
 
-        int ans =0;
-        for(ArrayList<Integer>a : arr){
-            int num =0;
-            for(int i=0; i<a.size(); i++){
-                num = num* 10 + a.get(i);
-            }
-            ans += num;
-        }
-        return ans;
+//         int ans =0;
+//         for(ArrayList<Integer>a : arr){
+//             int num =0;
+//             for(int i=0; i<a.size(); i++){
+//                 num = num* 10 + a.get(i);
+//             }
+//             ans += num;
+//         }
+//         return ans;
+                return traverse(root, 0);
+
         
+    }
+    public int traverse(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        sum = sum * 10 + root.val;
+        
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        
+        return traverse(root.left, sum) + traverse(root.right, sum);
     }
     public void solve(ArrayList<ArrayList<Integer>> arr, ArrayList<Integer> tba,TreeNode root){
         if(root == null){
