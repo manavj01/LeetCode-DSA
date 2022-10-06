206387ba6083b10a9618a11852257119fc31b30f
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-06 23:22:07 +0530

    Time: 20 ms (11.18%), Space: 89.6 MB (61.89%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -15,31 +15,45 @@
  */
 class Solution {
 
-    public int minDepth(TreeNode root) {
-        if(root == null) return 0;
-        ArrayDeque<TreeNode> dque = new ArrayDeque<>();
-        dque.addLast(root);
-        int level =1;
-        while(!dque.isEmpty()){
-            int size = dque.size();
+//     public int minDepth(TreeNode root) {
+//         if(root == null) return 0;
+//         ArrayDeque<TreeNode> dque = new ArrayDeque<>();
+//         dque.addLast(root);
+//         int level =1;
+//         while(!dque.isEmpty()){
+//             int size = dque.size();
             
-            for(int i=0; i<size; i++){
-                TreeNode parent = dque.pollFirst();
-                if(parent.left == null && parent.right == null){
-                    return level;
-                }
-                if(parent.left != null){
-                    dque.addLast(parent.left);
-                }
-                if(parent.right != null){
-                    dque.addLast(parent.right);
-                }
-            }
-            level++;
+//             for(int i=0; i<size; i++){
+//                 TreeNode parent = dque.pollFirst();
+//                 if(parent.left == null && parent.right == null){
+//                     return level;
+//                 }
+//                 if(parent.left != null){
+//                     dque.addLast(parent.left);
+//                 }
+//                 if(parent.right != null){
+//                     dque.addLast(parent.right);
+//                 }
+//             }
+//             level++;
+//         }
+        
+        
+//         return level;
+//     }
+    public int minDepth(TreeNode root){
+        if(root == null) return 0;
+        
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        
+        if(left == 0 || right == 0){
+            return left+right+1;
+        }else{
+        return Math.min(left,right)+1;
+            
         }
         
-        
-        return level;
     }
 
    
