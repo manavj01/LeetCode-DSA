181058a0a2cb0383a386b6756d301f8a7c3f5b4c
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-06 23:16:32 +0530

    Time: 2 ms (85.82%), Space: 86 MB (84.68%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        ArrayDeque<TreeNode> dque = new ArrayDeque<>();
+        dque.addLast(root);
+        int level =1;
+        while(!dque.isEmpty()){
+            int size = dque.size();
+            
+            for(int i=0; i<size; i++){
+                TreeNode parent = dque.pollFirst();
+                if(parent.left == null && parent.right == null){
+                    return level;
+                }
+                if(parent.left != null){
+                    dque.addLast(parent.left);
+                }
+                if(parent.right != null){
+                    dque.addLast(parent.right);
+                }
+            }
+            level++;
+        }
+        
+        
+        return level;
+    }
+
+   
+    
+    
+}
