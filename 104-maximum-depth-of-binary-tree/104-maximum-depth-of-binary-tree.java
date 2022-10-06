f629ef85c8dc5f58ae0fb247252e8d1f3cc525ff
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-06 23:31:57 +0530

    Time: 2 ms (9.69%), Space: 43.1 MB (54.19%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -14,16 +14,51 @@
  * }
  */
 class Solution {
+    // public int maxDepth(TreeNode root) {
+    //   if(root == null) return 0;
+    //     // Hypothesis
+    //     int left = maxDepth(root.left);
+    //     int right = maxDepth(root.right);
+    //     // Induction
+    //     return Math.max(left, right) + 1;
+    // }
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
-        
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        
-        if(left == 0 || right == 0){
-            return left+right+1;
-        }else{
-        return Math.max(left,right)+1;
+        ArrayDeque<TreeNode> dque = new ArrayDeque<>();
+        dque.addLast(root);
+        int level =0;
+        while(!dque.isEmpty()){
+            int size = dque.size();
+            
+            for(int i=0; i<size; i++){
+                TreeNode parent = dque.pollFirst();
+                // if(parent.left == null && parent.right == null){
+                //     return level;
+                // }
+                if(parent.left != null){
+                    dque.addLast(parent.left);
+                }
+                if(parent.right != null){
+                    dque.addLast(parent.right);
+                }
+            }
+            level++;
         }
+        
+        
+        return level;
     }
+//  public int maxDepth(TreeNode root){
+//         if(root == null) return 0;
+        
+//         int left = maxDepth(root.left);
+//         int right = maxDepth(root.right);
+        
+//         if(left == 0 || right == 0){
+//             return left+right+1;
+//         }else{
+//         return Math.max(left,right)+1;
+//         }
+        
+//     }
 }
