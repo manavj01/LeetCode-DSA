d1ba669d063a3ffc29713f23f68d5d40aeab87b6
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-07 16:28:56 +0530

    Time: 9 ms (98.11%), Space: 44.6 MB (96.32%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,43 +1,33 @@
 class Solution {
-
-    class Pair implements Comparable<Pair> {
-        int val;
-        int times;
-
-        Pair(int val, int times) {
-            this.val = val;
-            this.times = times;
-        }
-
-        public int compareTo(Pair o) {
-            return o.times - this.times;
-        }
-    }
-
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket = new List[nums.length + 1];
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        int[] result = new int[k];
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0)+1);
         }
-        int[] res = new int[k];
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int num = entry.getKey();
-            int times = entry.getValue();
-            if (bucket[times] == null) {
-                bucket[times] = new ArrayList<>();
-            }
-            bucket[times].add(num);
+        
+        PriorityQueue<Pair> pque = new PriorityQueue<>();
+        
+        for(int key : map.keySet()){
+            pque.add(new Pair(map.get(key),key));
         }
-        for (int i = nums.length; i > 0; i--) {
-            if (bucket[i] != null) {
-                for (int a : bucket[i])
-                    res[--k] = a;
-            }
-            if (k == 0) break;
+        while(k-- >0){
+            result[k] = pque.poll().b;
         }
-
-        return res;
+        
+        return result;
+    }
+    class Pair implements Comparable<Pair>{
+        int a;
+        int b;
+        Pair(int a , int b){
+            this.a = a;
+            this.b = b;
+        }
+        public int compareTo(Pair o){
+            return o.a-this.a;
+        }
     }
 }
