825a7cdac79038e735215ec12c8754fa8c713393
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-09 20:25:44 +0530

    Time: 1 ms (91.94%), Space: 42 MB (56.46%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,37 +1,16 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-         StringBuilder sb = new StringBuilder();
-//        Arrays.sort(strs, Comparator.comparingInt(String::length));
-        // System.out.println(Arrays.toString(strs));
-        int minLen = Integer.MAX_VALUE;
-        int minIdx = -1;
-        for (int i = 0; i < strs.length; i++) {
-            if (strs[i].length() < minLen) {
-                minLen = strs[i].length();
-                minIdx = i;
+        
+        String prefix = strs[0];
+
+        for(int i=1; i<strs.length; i++){
+            
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0,prefix.length()-1);
+                
             }
         }
-        for (int i = 0; i < minLen; i++) {
-            char ch = strs[minIdx].charAt(i);
-            for (String str : strs) {
-                if (ch != str.charAt(i)) {
-                    return sb.toString();
-                }
-            }
-            sb.append(ch);
-        }
-
-        return sb.toString();
-
+        
+        return prefix;
     }
-    //  public static String longestCommonPrefix(String[] strs) {
-    //     if (strs.length == 0) return "";
-    //     String prefix = strs[0];
-    //     for (String str : strs) {
-    //         while (str.indexOf(prefix) != 0) {
-    //             prefix = prefix.substring(0, prefix.length() - 1);
-    //         }
-    //     }
-    //     return prefix;
-    // }
 }
