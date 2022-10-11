472f59815bc95e2a1df8883142f35f18974186e1
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-11 22:40:24 +0530

    Time: 214 ms (5.02%), Space: 94.9 MB (14.65%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public boolean increasingTriplet(int[] nums) {
+                HashMap<Integer,Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, 0);
+        }
+        if (map.size()==2) return false;
+        int c=1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int val = nums[i];
+            c = 1;
+            int cc =1;
+            int spare = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > val) {
+                    c++;
+                    val = nums[j];
+                }
+                if(nums[j] > spare && nums[j] != val ){
+                    cc++;
+                    spare = nums[j];
+                }
+            if (c >= 3) return true;
+            if (cc >=3) return true;
+            }
+     
+        }
+//        System.out.println(c);
+        return false;
+    }
+}
