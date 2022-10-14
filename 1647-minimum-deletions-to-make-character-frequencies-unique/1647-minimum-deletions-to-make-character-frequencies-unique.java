f8535498de13de45865da23e87f1a50590b3d2a1
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-14 11:18:06 +0530

    Time: 8 ms (100.00%), Space: 42.9 MB (95.56%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,27 +1,44 @@
 class Solution {
 
     public int minDeletions(String s) {
-        int[] freq = new int[26];
+//         int[] freq = new int[26];
 
-        for (int i = 0; i < s.length(); i++) {
-            ++freq[s.charAt(i) - 'a'];
-        }
+//         for (int i = 0; i < s.length(); i++) {
+//             ++freq[s.charAt(i) - 'a'];
+//         }
         
-        int c = 0;
-        for (int i = 25; i > 0; i--) {
-            for (int j = 25; j > 0; j--) {
-                Arrays.sort(freq);
+//         int c = 0;
 
-                if (freq[j] == 0 && freq[j - 1] == 0) break;
+//         for (int i = 25; i > 0; i--) {
+//             for (int j = 25; j > 0; j--) {
+//                 Arrays.sort(freq);
+//                 if (freq[j] == 0 && freq[j - 1] == 0) break;
+//                 if (freq[j] == freq[j - 1]) {
+//                     c++;
+//                     freq[j - 1]--;
+//                 }
+//             }
+//         }
+        
 
-                if (freq[j] == freq[j - 1]) {
-                    c++;
-                    freq[j - 1]--;
-                }
+//         return c;
+         int[] arr = new int[26];
+        int res = 0;
+        for (char ch : s.toCharArray()) {
+            arr[ch - 'a']++;
+        }
+        Set<Integer> used = new HashSet<>();
+        for (int i = 0; i < 26; i++) {
+            int freq = arr[i];
+            while (freq > 0) {
+                if (!used.contains(freq)) {
+                    used.add(freq);
+                    break;
+                } 
+                freq--;
+                res++;
             }
         }
-        
-
-        return c;
+        return res; 
     }
 }
