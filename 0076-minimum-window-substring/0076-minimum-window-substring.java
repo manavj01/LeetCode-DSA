2c7d888ea2bee7e1b0fa4d8dadfc774ab75b7e9b
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-25 20:23:08 +0530

    Time: 28 ms (44.87%), Space: 44.4 MB (56.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,91 +1,54 @@
-// class Solution {
+class Solution {
 
-//     public String minWindow(String s, String t) {
-//         HashMap<Character, Integer> map = new HashMap<>();
-
-//         for (int i = 0; i < t.length(); i++) {
-//             map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
-//         }
-        
-//         int length = s.length();
-//         int left = 0, right = 0, ansLeft = 0, ansRight = 0;
-        
-//         int counter = t.length();
-        
-//         boolean flag = false;
-        
-        
-        class Solution {
     public String minWindow(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        if (s.length() == 0 || t.length() == 0) {
-            return "";
-        }
-
-        // Dictionary which keeps a count of all the unique characters in t.
-        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
         for (int i = 0; i < t.length(); i++) {
-            int count = dictT.getOrDefault(t.charAt(i), 0);
-            dictT.put(t.charAt(i), count + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
         }
-
-        // Number of unique characters in t, which need to be present in the desired window.
-        int required = dictT.size();
-
-        // Left and Right pointer
-        int l = 0, r = 0;
-
-        // formed is used to keep track of how many unique characters in t
-        // are present in the current window in its desired frequency.
-        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
-        // Thus formed would be = 3 when all these conditions are met.
-        int formed = 0;
-
-        // Dictionary which keeps a count of all the unique characters in the current window.
-        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
-
-        // ans list of the form (window length, left, right)
-        int[] ans = { -1, 0, 0 };
-
-        while (r < s.length()) {
-            // Add one character from the right to the window
-            char c = s.charAt(r);
-            int count = windowCounts.getOrDefault(c, 0);
-            windowCounts.put(c, count + 1);
-
-            // If the frequency of the current character added equals to the
-            // desired count in t then increment the formed count by 1.
-            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-                formed++;
-            }
-
-            // Try and contract the window till the point where it ceases to be 'desirable'.
-            while (l <= r && formed == required) {
-                c = s.charAt(l);
-                // Save the smallest window until now.
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+        
+        int length = s.length();
+        int left = 0, right = 0, ansLeft = 0, ansRight = 0;
+        
+        int counter = t.length();
+        
+        boolean flag = false;
+        
+        int min = Integer.MAX_VALUE;
+        
+        while(right < length){
+            char ch = s.charAt(right);
+            
+            if(map.containsKey(ch)){
+                if(map.get(ch)> 0){
+                    counter--;
                 }
-
-                // The character at the position pointed by the
-                // `Left` pointer is no longer a part of the window.
-                windowCounts.put(c, windowCounts.get(c) - 1);
-                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                    formed--;
-                }
-
-                // Move the left pointer ahead, this would help to look for a new window.
-                l++;
+                map.put(ch, map.getOrDefault(ch,0)-1);
             }
-
-            // Keep expanding the window once we are done contracting.
-            r++;
+            
+            while(counter == 0 && left <= right){
+                if(right-left+1 < min){
+                    ansLeft = left;
+                    ansRight = right;
+                    min = right-left+1;
+                }
+                if(left == right) break;
+                
+                ch = s.charAt(left);
+                
+                if(map.containsKey(ch)){
+                    map.put(ch, map.getOrDefault(ch,0)+1);
+                    
+                    if(map.get(ch) > 0){
+                        counter++;
+                    }
+                }
+                left++;
+            }
+            right++;
         }
-
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        
+        
+        return min == Integer.MAX_VALUE ? "" : s.substring(ansLeft,ansRight+1);
     }
 }
-        
-    
