27020df8082d683071b65972ae3bac61289b73fe
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-27 15:56:27 +0530

    Time: 12 ms (97.88%), Space: 78.9 MB (66.82%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-       int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         for (int num : nums) {
             if (num < min) min = num;
             if (num > max) max = num;
@@ -16,4 +16,42 @@ class Solution {
         
         return nums; 
     }
+    // quicksort solution
+//      public int[] sortArray(int[] nums) {
+//         quickSort(nums, 0 , nums.length-1);
+//         return nums;
+//     }
+//     public  void quickSort(int[] arr , int lo, int hi ){
+//         if(lo > hi){
+//             return;
+//         }
+//         int pivot = arr[hi];
+//         int pivotIdx = partition(arr, pivot, lo , hi);
+        
+//         quickSort(arr, lo, pivotIdx-1);        
+//         quickSort(arr,  pivotIdx+1, hi);
+
+        
+//     }
+//     public int partition(int[] arr , int pivot, int lo , int hi){
+//         int i = lo , j = lo; 
+        
+//         while(i<= hi){
+//             if(arr[i] <= pivot){
+//                 swap(arr, i, j);
+//                i++;
+//                j++;
+//             }else {
+//                 i++;
+//             }
+//         }
+        
+//         return (j-1);
+//     }
+//     public void swap (int[] arr , int i , int j){
+//         if(i==j) return;
+//         int temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp ;
+//     }
 }
