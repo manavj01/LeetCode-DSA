7333b58e6c97ecbbcf30eaf6445bcfb1c9a0cdae
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-29 21:43:31 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (91.05%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,45 +10,32 @@
  */
 class Solution {
 
-    //     public ListNode swapPairs(ListNode head) {
-    //         int c = 0;
-
-    //         if (head == null || head.next == null) return head;
-    //         ListNode temp1 = head;
-
-    //         while (temp1 != null) {
-    //             temp1 = temp1.next;
-    //             c++;
-    //         }
-    //         temp1 = head;
-    //         ListNode temp2 = head.next;
-
-    //         ListNode dummy = new ListNode(-1, head);
-
-    //         int size = c / 2;
-    //         while (c-- > 0) {
-    //             temp1.next = temp2.next;
-    //             temp2.next = temp1;
-    //         }
-
-    //         return dummy.next;
-    //     }
     public ListNode swapPairs(ListNode head) {
-        
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode node = swapPairs(head.next.next);
+
+        ListNode cur = head.next;
+
+        cur.next = head;
+
+        head.next = node;
+
+        return cur;
+    }
+
+    public ListNode swapPairs1(ListNode head) {
         if (head == null || head.next == null) return head;
         ListNode temp1 = head;
         ArrayList<ListNode> arr = new ArrayList<>();
         while (temp1 != null) {
-            arr.add(temp1) ;
+            arr.add(temp1);
             temp1 = temp1.next;
-            
         }
-     
         for (int i = 0, j = 1; i < j && j < arr.size(); i += 2, j += 2) {
-            Collections.swap(arr,i,j);
-            // ListNode t = arr[i];
-            // arr[i] = arr[j];
-            // arr[j] = t;
+            Collections.swap(arr, i, j);
         }
         for (int i = 1; i < arr.size(); i++) {
             arr.get(i - 1).next = arr.get(i);
