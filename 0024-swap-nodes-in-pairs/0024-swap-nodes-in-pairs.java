7de6989121818327f732cb2fb54dfd154e6e2fe9
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-10-29 21:42:09 +0530

    Time: 1 ms (5.38%), Space: 41.4 MB (70.42%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,57 +10,50 @@
  */
 class Solution {
 
-//     public ListNode swapPairs(ListNode head) {
-//         int c = 0;
+    //     public ListNode swapPairs(ListNode head) {
+    //         int c = 0;
 
-//         if (head == null || head.next == null) return head;
-//         ListNode temp1 = head;
+    //         if (head == null || head.next == null) return head;
+    //         ListNode temp1 = head;
 
-//         while (temp1 != null) {
-//             temp1 = temp1.next;
-//             c++;
-//         }
-//         temp1 = head;
-//         ListNode temp2 = head.next;
+    //         while (temp1 != null) {
+    //             temp1 = temp1.next;
+    //             c++;
+    //         }
+    //         temp1 = head;
+    //         ListNode temp2 = head.next;
 
-//         ListNode dummy = new ListNode(-1, head);
+    //         ListNode dummy = new ListNode(-1, head);
 
-//         int size = c / 2;
-//         while (c-- > 0) {
-//             temp1.next = temp2.next;
-//             temp2.next = temp1;
-//         }
+    //         int size = c / 2;
+    //         while (c-- > 0) {
+    //             temp1.next = temp2.next;
+    //             temp2.next = temp1;
+    //         }
 
-//         return dummy.next;
-//     }
-      public ListNode swapPairs(ListNode head) {
-        int c = 0;
-
+    //         return dummy.next;
+    //     }
+    public ListNode swapPairs(ListNode head) {
+        
         if (head == null || head.next == null) return head;
         ListNode temp1 = head;
-
+        ArrayList<ListNode> arr = new ArrayList<>();
         while (temp1 != null) {
+            arr.add(temp1) ;
             temp1 = temp1.next;
-            c++;
+            
         }
-          
-          ListNode[] arr = new ListNode[c];
-          temp1 = head;
-          int idx =0;
-          while(temp1 != null){
-              arr[idx++] = temp1;
-              temp1 = temp1.next;
-          }
-          
-          for(int i=0, j=1; i<j && j<arr.length; i+=2, j+=2){
-              ListNode t = arr[i];
-              arr[i] = arr[j];
-              arr[j] = t;
-          }
-          for(int i=1; i<arr.length; i++){
-              arr[i-1].next = arr[i];
-          }
-       arr[arr.length-1].next = null;
-        return arr[0];
+     
+        for (int i = 0, j = 1; i < j && j < arr.size(); i += 2, j += 2) {
+            Collections.swap(arr,i,j);
+            // ListNode t = arr[i];
+            // arr[i] = arr[j];
+            // arr[j] = t;
+        }
+        for (int i = 1; i < arr.size(); i++) {
+            arr.get(i - 1).next = arr.get(i);
+        }
+        arr.get(arr.size() - 1).next = null;
+        return arr.get(0);
     }
 }
