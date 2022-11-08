f1ff0b9421c2582ed00e1fc3d899bc6597010007
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-11-08 22:53:27 +0530

    Time: 3 ms (93.08%), Space: 41 MB (95.76%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,27 +1,44 @@
 class Solution {
 
     public String makeGood(String s) {
-        StringBuilder sb = new StringBuilder(s);
-//         if(sb.length() == 2 && ) return "";
+//         StringBuilder sb = new StringBuilder(s);
+// //         if(sb.length() == 2 && ) return "";
         
-        while (true) {
-            for (int i = 0; i < sb.length() - 1; i++) {
-                if ( (sb.charAt(i) == sb.charAt(i+ 1) + 32) || (sb.charAt(i) + 32 == sb.charAt(i+ 1)) ) {
-                    sb.delete(i, i + 2);
-                    break;
-                }
-            }
-            boolean flag = false;
-            for (int i = 0; i < sb.length() - 1; i++) {
-                if (( (sb.charAt(i) == sb.charAt( i+1) + 32) || (sb.charAt(i)+32 == sb.charAt( i+1)))) {
-                   flag = true;
-                }
-            }
-            if(!flag || sb.length() == 0){
-                return sb.toString();
+//         while (true) {
+//             for (int i = 0; i < sb.length() - 1; i++) {
+//                 if ( (sb.charAt(i) == sb.charAt(i+ 1) + 32) || (sb.charAt(i) + 32 == sb.charAt(i+ 1)) ) {
+//                     sb.delete(i, i + 2);
+//                     break;
+//                 }
+//             }
+//             boolean flag = false;
+//             for (int i = 0; i < sb.length() - 1; i++) {
+//                 if (( (sb.charAt(i) == sb.charAt( i+1) + 32) || (sb.charAt(i)+32 == sb.charAt( i+1)))) {
+//                    flag = true;
+//                 }
+//             }
+//             if(!flag || sb.length() == 0){
+//                 return sb.toString();
+//             }
+//         }
+        
+//         // return s;
+        Stack<Character> stack = new Stack<>();
+        
+        for(char ch : s.toCharArray()){
+            if(!stack.isEmpty() && Math.abs(stack.peek() - ch) == 32){
+                stack.pop();
+            }else{
+                stack.push(ch);
             }
         }
         
-        // return s;
+        StringBuilder ans = new StringBuilder();
+        
+        for(char curr : stack){
+            ans.append(curr);
+        }
+        
+        return ans.toString();
     }
 }
