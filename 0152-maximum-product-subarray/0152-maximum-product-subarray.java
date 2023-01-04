69dccf01af1d82a0e346237bdc4fa2f58e54c777
Manav Jain <67707554+manavj01@users.noreply.github.com>
2023-01-04 19:49:38 +0530

    Time: 1 ms (95.42%), Space: 44.8 MB (38.48%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,76 +1,76 @@
 class Solution {
 
     public int maxProduct(int[] nums) {
-        //         int product=1;
-        //         int max=Integer.MIN_VALUE;
+                int product=1;
+                int max=Integer.MIN_VALUE;
 
-        //         for(int i=0;i<=nums.length-1;i++){
-        //             product*=nums[i];
-        //             max=Math.max(max,product);
-        //             if(product==0)
-        //                 product=1;
-        //         }
+                for(int i=0;i<=nums.length-1;i++){
+                    product*=nums[i];
+                    max=Math.max(max,product);
+                    if(product==0)
+                        product=1;
+                }
 
-        //         product=1;
+                product=1;
 
-        //         for(int i=nums.length-1;i>=0;i--){
-        //             product*=nums[i];
-        //             max=Math.max(max,product);
-        //             if(product==0)
-        //                 product=1;
-        //         }
-        //         return max;
+                for(int i=nums.length-1;i>=0;i--){
+                    product*=nums[i];
+                    max=Math.max(max,product);
+                    if(product==0)
+                        product=1;
+                }
+                return max;
 
-        int last = 1;
-        int prod = 1;
-        int max = Integer.MIN_VALUE;
-// -1,-2,-3 -> 
-        for (int i = 0; i < nums.length; i++) {
+//         int last = 1;
+//         int prod = 1;
+//         int max = Integer.MIN_VALUE;
+// // -1,-2,-3 -> 
+//         for (int i = 0; i < nums.length; i++) {
             
-            prod *= nums[i];
+//             prod *= nums[i];
             
-            max = Math.max(max, prod);
-            if(prod==0){
-                prod = 1;
-                last = 1;
-                continue;
-            }
-            if(last < 0 && prod < 0 ){
-                prod *= last;
-                last = 0;
-                 max = Math.max(max, prod);
+//             max = Math.max(max, prod);
+//             if(prod==0){
+//                 prod = 1;
+//                 last = 1;
+//                 continue;
+//             }
+//             if(last < 0 && prod < 0 ){
+//                 prod *= last;
+//                 last = 0;
+//                  max = Math.max(max, prod);
 
-            }
-            if (prod < 0) {
-                last = prod;
-                prod = 1;
-            }
-        }
-        prod = 1;
-        last = 1;
-        for (int i = nums.length-1; i >=0; i--) {
+//             }
+//             if (prod < 0) {
+//                 last = prod;
+//                 prod = 1;
+//             }
+//         }
+//         prod = 1;
+//         last = 1;
+//         for (int i = nums.length-1; i >=0; i--) {
             
-            prod *= nums[i];
-            max = Math.max(max, prod);
-            if(prod==0){
-                prod = 1;
-                last = 1;
-                continue;
-            }
-            if(last < 0 && prod < 0 ){
-                prod *= last;
-                last = 0;
-                 max = Math.max(max, prod);
+//             prod *= nums[i];
+//             max = Math.max(max, prod);
+//             if(prod==0){
+//                 prod = 1;
+//                 last = 1;
+//                 continue;
+//             }
+//             if(last < 0 && prod < 0 ){
+//                 prod *= last;
+//                 last = 0;
+//                  max = Math.max(max, prod);
 
-            }
-            if (prod < 0) {
-                last = prod;
-                prod = 1;
-            }
+//             }
+//             if (prod < 0) {
+//                 last = prod;
+//                 prod = 1;
+//             }
             
-        }
+//         }
 
-        return max;
+//         return max;
         //         int l = 1;
         //         int r = 1;
         //         int max =Integer.MIN_VALUE;
@@ -83,4 +83,5 @@ class Solution {
         //         // [2,-5,-2,-4,3]
         //         return max;
     }
+    
 }
